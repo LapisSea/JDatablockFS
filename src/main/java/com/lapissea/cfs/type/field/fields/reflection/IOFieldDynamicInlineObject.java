eb13290b7c6a3ea24ffad3d2b76e7771b036eece
LapisSea <officiallapisseamail@gmail.com>
2021-11-04 06:09:13 +0100

    Add support for Unmanged instance value in IOFieldDynamicInlineObject

--- src/main/java/com/lapissea/cfs/type/field/fields/reflection/IOFieldDynamicInlineObject.java
+++ src/main/java/com/lapissea/cfs/type/field/fields/reflection/IOFieldDynamicInlineObject.java
@@ -27,7 +27,8 @@ import java.util.OptionalLong;
 
 public class IOFieldDynamicInlineObject<CTyp extends IOInstance<CTyp>, ValueType> extends IOField<CTyp, ValueType>{
 	
-	private static final StructPipe<AutoText> STR_DESC=ContiguousStructPipe.of(AutoText.class);
+	private static final StructPipe<AutoText>  STR_PIPE=ContiguousStructPipe.of(AutoText.class);
+	private static final StructPipe<Reference> REF_PIPE=ContiguousStructPipe.of(Reference.class);
 	
 	private final SizeDescriptor<CTyp> descriptor;
 	
@@ -70,8 +71,14 @@ public class IOFieldDynamicInlineObject<CTyp extends IOInstance<CTyp>, ValueType
 			case Float ignored -> 4;
 			case Long ignored -> 8;
 			case Double ignored -> 8;
-			case String str -> STR_DESC.getSizeDescriptor().calcUnknown(new AutoText(str), WordSpace.BYTE);
-			case IOInstance inst -> ContiguousStructPipe.sizeOfUnknown(inst, WordSpace.BYTE);
+			case String str -> STR_PIPE.getSizeDescriptor().calcUnknown(new AutoText(str), WordSpace.BYTE);
+			case IOInstance inst -> {
+				if(inst instanceof IOInstance.Unmanaged<?> u){
+					yield REF_PIPE.getSizeDescriptor().calcUnknown(u.getReference(), WordSpace.BYTE);
+				}else{
+					yield ContiguousStructPipe.sizeOfUnknown(inst, WordSpace.BYTE);
+				}
+			}
 			default -> throw new NotImplementedException(val.getClass()+"");
 		};
 	}
@@ -83,8 +90,12 @@ public class IOFieldDynamicInlineObject<CTyp extends IOInstance<CTyp>, ValueType
 			case Float v -> NumberSize.INT.writeFloating(dest, v);
 			case Long v -> NumberSize.LONG.write(dest, v);
 			case Double v -> NumberSize.LONG.writeFloating(dest, v);
-			case String str -> STR_DESC.write(provider, dest, new AutoText(str));
+			case String str -> STR_PIPE.write(provider, dest, new AutoText(str));
+			case IOInstance.Unmanaged inst -> {
+				REF_PIPE.write(provider, dest, inst.getReference());
+			}
 			case IOInstance inst -> ContiguousStructPipe.of(inst.getThisStruct()).write(provider, dest, inst);
+			
 			default -> throw new NotImplementedException(val.getClass()+"");
 		}
 	}
@@ -95,8 +106,17 @@ public class IOFieldDynamicInlineObject<CTyp extends IOInstance<CTyp>, ValueType
 		if(typ==Float.class) return (float)NumberSize.INT.readFloating(src);
 		if(typ==Long.class) return NumberSize.LONG.read(src);
 		if(typ==Double.class) return NumberSize.LONG.readFloating(src);
-		if(typ==String.class) return STR_DESC.readNew(provider, src, genericContext).getData();
-		if(UtilL.instanceOf(typ, IOInstance.class)) return readStruct(provider, src, genericContext, Struct.ofUnknown(typ));
+		if(typ==String.class) return STR_PIPE.readNew(provider, src, genericContext).getData();
+		if(UtilL.instanceOf(typ, IOInstance.Unmanaged.class)){
+			var uStruct=Struct.Unmanaged.ofUnknown(typ);
+			var ref    =REF_PIPE.readNew(provider, src, genericContext);
+			var inst   =uStruct.requireUnmanagedConstructor().create(provider, ref, typDef);
+			return inst;
+		}
+		if(UtilL.instanceOf(typ, IOInstance.class)){
+			var struct=Struct.ofUnknown(typ);
+			return readStruct(provider, src, genericContext, struct);
+		}
 		throw new NotImplementedException(typ+"");
 	}
 	private void skipReadTyp(TypeDefinition typDef, ChunkDataProvider provider, ContentReader src, GenericContext genericContext) throws IOException{
@@ -111,11 +131,15 @@ public class IOFieldDynamicInlineObject<CTyp extends IOInstance<CTyp>, ValueType
 		}
 		
 		if(typ==String.class){
-			STR_DESC.readNew(provider, src, genericContext);
+			STR_PIPE.readNew(provider, src, genericContext);
 			return;
 		}
 		if(UtilL.instanceOf(typ, IOInstance.class)){
-			skipReadStruct(provider, src, genericContext, Struct.ofUnknown(typ));
+			if(UtilL.instanceOf(typ, IOInstance.Unmanaged.class)){
+				REF_PIPE.readNew(provider, src, genericContext);
+			}else{
+				skipReadStruct(provider, src, genericContext, Struct.ofUnknown(typ));
+			}
 		}
 		
 		throw new NotImplementedException(typ+"");
@@ -171,7 +195,13 @@ public class IOFieldDynamicInlineObject<CTyp extends IOInstance<CTyp>, ValueType
 			dest.writeBoolean(true);
 		}
 		
-		int id=provider.getTypeDb().toID(val.getClass());
+		TypeDefinition def;
+		if(val instanceof IOInstance.Unmanaged<?> unmanaged){
+			def=unmanaged.getTypeDef();
+		}else{
+			def=TypeDefinition.of(val.getClass());
+		}
+		int id=provider.getTypeDb().toID(def);
 		dest.writeInt4(id);
 		
 		writeValue(provider, dest, val);
