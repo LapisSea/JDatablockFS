0d3f54bd6e26eac750f5b6f9e7e01a77e73e3305
LapisSea <officiallapisseamail@gmail.com>
2021-12-01 22:30:33 +0100

    Separate tests

--- jdatablockfs.run/src/test/java/com/lapisseqa/cfs/run/GeneralTests.java
+++ jdatablockfs.run/src/test/java/com/lapisseqa/cfs/run/GeneralTests.java
@@ -4,8 +4,6 @@ import com.lapissea.cfs.chunk.AllocateTicket;
 import com.lapissea.cfs.chunk.Chunk;
 import com.lapissea.cfs.chunk.Cluster;
 import com.lapissea.cfs.chunk.DataProvider;
-import com.lapissea.cfs.io.IOInterface;
-import com.lapissea.cfs.io.impl.MemoryData;
 import com.lapissea.cfs.io.instancepipe.ContiguousStructPipe;
 import com.lapissea.cfs.io.instancepipe.StructPipe;
 import com.lapissea.cfs.objects.Reference;
@@ -28,7 +26,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HexFormat;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -61,73 +58,6 @@ public class GeneralTests{
 				dummy.write(1);
 			}
 		}
-	}
-	
-	@Test
-	void ioTransaction() throws IOException{
-		int cap=50;
-		
-		IOInterface data  =MemoryData.build().withCapacity(cap+10).build();
-		IOInterface mirror=MemoryData.build().withCapacity(cap+10).build();
-		
-		
-		int runIndex=0;
-		
-		var rand=new Random(1);
-		//Dumb brute force all possible edge cases
-		for(int run=0;run<100000;run++){
-			if(run%10000==0) LogUtil.println(run);
-			
-			try(var ignored=data.openIOTransaction()){
-				var runSize=rand.nextInt(40);
-				for(int j=1;j<runSize+1;j++){
-					runIndex++;
-					
-					int off=rand.nextInt(cap);
-					int siz=rand.nextInt(10);
-					
-					byte[] buf=new byte[siz];
-					Arrays.fill(buf, (byte)j);
-
-//					LogUtil.println("================================");
-					
-					mirror.write(off, false, buf);
-//					LogUtil.println("  ", HexFormat.of().formatHex(mirror.readAll()).replace('0', '.'));
-//					LogUtil.println(HexFormat.of().formatHex(buf).replace('0', '.'));
-
-//					LogUtil.println("a ", HexFormat.of().formatHex(data.readAll()).replace('0', '.'));
-					data.write(off, false, buf);
-//					LogUtil.println("b ", HexFormat.of().formatHex(data.readAll()).replace('0', '.'));
-					
-					for(int i=0;i<100;i++){
-						var rSiz=rand.nextInt(20);
-						var rOff=rand.nextInt(cap+10-rSiz);
-						assertArrayEquals(mirror.read(rOff, rSiz),
-						                  data.read(rOff, rSiz), ""+i);
-					}
-					
-					assertArrayEquals(mirror.read(off+1, 9),
-					                  data.read(off+1, 9));
-					check(mirror, data, "failed on run "+runIndex);
-				}
-			}catch(Throwable e){
-				throw new RuntimeException("failed on run "+runIndex, e);
-			}
-			check(mirror, data, "failed after cycle "+run);
-		}
-	}
-	private void check(IOInterface expected, IOInterface data, String s) throws IOException{
-		byte[] m, d;
-		try{
-			m=expected.readAll();
-			d=data.readAll();
-		}catch(Throwable e){
-			throw new RuntimeException(s, e);
-		}
-		assertArrayEquals(m, d, ()->s+"\n"+
-		                            HexFormat.of().formatHex(m)+"\n"+
-		                            HexFormat.of().formatHex(d)+"\n"
-		);
 	}
 	
 	@ParameterizedTest
