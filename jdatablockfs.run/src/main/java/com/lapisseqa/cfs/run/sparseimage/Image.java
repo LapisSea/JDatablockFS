fad21cf9fa8ba1596fcb6508cfdd66f358e14f91
LapisSea <officiallapisseamail@gmail.com>
2022-02-10 14:29:11 +0100

    Update sparse image

--- jdatablockfs.run/src/main/java/com/lapisseqa/cfs/run/sparseimage/Image.java
+++ jdatablockfs.run/src/main/java/com/lapisseqa/cfs/run/sparseimage/Image.java
@@ -9,6 +9,23 @@ import java.io.IOException;
 
 public class Image extends IOInstance<Image>{
 	
+	public static class Pixel extends IOInstance<Pixel>{
+		@IOValue
+		private float r;
+		@IOValue
+		private float g;
+		@IOValue
+		private float b;
+		
+		public Pixel(){}
+		
+		public Pixel(float r, float g, float b){
+			this.r=r;
+			this.g=g;
+			this.b=b;
+		}
+	}
+	
 	private static final int chunkSize=2;
 	
 	@IOValue
@@ -22,7 +39,7 @@ public class Image extends IOInstance<Image>{
 		int chunkY=y/chunkSize;
 		
 		var  dist    =Double.MAX_VALUE;
-		long addIndex=-1;
+		long addIndex=0;
 		
 		var iter=chunks.listIterator();
 		while(iter.hasNext()){
@@ -42,20 +59,14 @@ public class Image extends IOInstance<Image>{
 			int localY=y-chunkY*chunkSize;
 			int index =localX+localY*chunkSize;
 			
-			c.pixels[index*3+0]=r;
-			c.pixels[index*3+1]=g;
-			c.pixels[index*3+2]=b;
+			c.pixels[index]=new Pixel(r,g,b);
 			
 			iter.ioSet(c);
 			return;
 		}
 		
 		var ch=new Chunk(chunkX, chunkY, chunkSize);
-		if(addIndex!=-1){
-			chunks.add(addIndex, ch);
-		}else{
-			chunks.add(0, ch);
-		}
+		chunks.add(addIndex, ch);
 		set(x, y, r, g, b);
 	}
 }
