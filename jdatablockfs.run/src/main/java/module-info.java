e74e9e4f9cf6544bee3ab678f4862d9bfefc9542
LapisSea <officiallapisseamail@gmail.com>
2022-03-12 12:11:53 +0100

    Update deps, add JMH

--- jdatablockfs.run/src/main/java/module-info.java
+++ jdatablockfs.run/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module JDatablockFS.run {
 	requires JDatablockFS.tools;
 	requires jlapisutil;
 	requires Jorth;
+	requires jmh.core;
+	requires jmh.generator.annprocess;
 	requires gson;
 	
 	opens com.lapisseqa.cfs.run;
