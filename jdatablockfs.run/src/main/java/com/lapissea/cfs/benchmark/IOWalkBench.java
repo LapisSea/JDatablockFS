7b25a05bea657ea17d4b4e262d56fe4fcfff5579
LapisSea <officiallapisseamail@gmail.com>
2022-03-26 20:45:07 +0100

    Update IOWalkBench

--- jdatablockfs.run/src/main/java/com/lapissea/cfs/benchmark/IOWalkBench.java
+++ jdatablockfs.run/src/main/java/com/lapissea/cfs/benchmark/IOWalkBench.java
@@ -14,22 +14,46 @@ import com.lapissea.cfs.type.field.IOField;
 import org.openjdk.jmh.annotations.*;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Warmup(iterations=6, time=400, timeUnit=TimeUnit.MILLISECONDS)
-@Measurement(iterations=6, time=1000, timeUnit=TimeUnit.MILLISECONDS)
+@Measurement(iterations=6, time=500, timeUnit=TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class IOWalkBench{
 	
-	private final Cluster cluster;
+	public static void main(String[] args){
+		new IOWalkBench().doWalk();
+	}
+	
+	private final Cluster                    cluster;
+	private       MemoryWalker.PointerRecord rec=new MemoryWalker.PointerRecord(){
+		@Override
+		public <T extends IOInstance<T>> MemoryWalker.IterationOptions log(StructPipe<T> pipe, Reference instanceReference, IOField.Ref<T, ?> field, T instance, Reference value) throws IOException{
+			return MemoryWalker.IterationOptions.CONTINUE_NO_SAVE;
+		}
+		@Override
+		public <T extends IOInstance<T>> MemoryWalker.IterationOptions logChunkPointer(StructPipe<T> pipe, Reference instanceReference, IOField<T, ChunkPointer> field, T instance, ChunkPointer value) throws IOException{
+			return MemoryWalker.IterationOptions.CONTINUE_NO_SAVE;
+		}
+	};
 	
 	public IOWalkBench(){
 		try{
 			IOInterface mem=MemoryData.build().build();
 			Cluster.init(mem);
-			var c=new Cluster(mem);
-			SparseImage.run(c, new Configuration().getView());
+			var c   =new Cluster(mem);
+			var conf=new Configuration();
+			
+			Map<String, Object> vals=new HashMap<>();
+			if(System.getProperty("radius")!=null){
+				vals.put("radius", System.getProperty("radius"));
+			}
+			
+			conf.load(()->vals.entrySet().stream());
+			SparseImage.run(c, conf.getView());
 			
 			if("true".equals(System.getProperty("read_only_benchy"))){
 				mem=mem.asReadOnly();
@@ -44,16 +68,8 @@ public class IOWalkBench{
 	
 	public void doWalk(){
 		try{
-			cluster.rootWalker().walk(new MemoryWalker.PointerRecord(){
-				@Override
-				public <T extends IOInstance<T>> MemoryWalker.IterationOptions log(StructPipe<T> pipe, Reference instanceReference, IOField.Ref<T, ?> field, T instance, Reference value) throws IOException{
-					return MemoryWalker.IterationOptions.CONTINUE_NO_SAVE;
-				}
-				@Override
-				public <T extends IOInstance<T>> MemoryWalker.IterationOptions logChunkPointer(StructPipe<T> pipe, Reference instanceReference, IOField<T, ChunkPointer> field, T instance, ChunkPointer value) throws IOException{
-					return MemoryWalker.IterationOptions.CONTINUE_NO_SAVE;
-				}
-			});
+			var r=cluster.rootWalker();
+			r.walk(rec);
 		}catch(Exception e){
 			throw new RuntimeException(e);
 		}
@@ -61,6 +77,16 @@ public class IOWalkBench{
 	
 	@Benchmark
 	public void walk(){
+		doWalk();
+	}
+	@Benchmark
+	@Fork(jvmArgsAppend="-Dradius=2")
+	public void walk2(){
+		doWalk();
+	}
+	@Benchmark
+	@Fork(jvmArgsAppend="-Dradius=30")
+	public void walk30(){
 		doWalk();
 	}
 	
