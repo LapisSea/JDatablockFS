7491630c59519359b306b19a62976894a21ef42f
LapisSea <officiallapisseamail@gmail.com>
2022-06-02 12:51:05 +0200

    Add IOList.map

--- src/main/java/com/lapissea/cfs/objects/collections/IOList.java
+++ src/main/java/com/lapissea/cfs/objects/collections/IOList.java
@@ -9,8 +9,10 @@ import com.lapissea.util.function.UnsafeFunction;
 import java.io.IOException;
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 public interface IOList<T> extends IterablePP<T>{
@@ -83,8 +85,222 @@ public interface IOList<T> extends IterablePP<T>{
 		}
 	}
 	
+	abstract class MappedIOList<From, To> implements IOList<To>{
+		private final IOList<From> data;
+		
+		protected MappedIOList(IOList<From> data){
+			this.data=data;
+		}
+		
+		
+		protected abstract To map(From v);
+		protected abstract From unmap(To v);
+		
+		
+		@Override
+		public long size(){
+			return data.size();
+		}
+		@Override
+		public To get(long index) throws IOException{
+			var v=data.get(index);
+			return map(v);
+		}
+		
+		@Override
+		public void set(long index, To value) throws IOException{
+			data.set(index, unmap(value));
+		}
+		@Override
+		public void add(long index, To value) throws IOException{
+			data.add(index, unmap(value));
+		}
+		@Override
+		public void add(To value) throws IOException{
+			data.add(unmap(value));
+		}
+		@Override
+		public void remove(long index) throws IOException{
+			data.remove(index);
+		}
+		@Override
+		public To addNew(UnsafeConsumer<To, IOException> initializer) throws IOException{
+			return map(data.addNew(from->initializer.accept(map(from))));
+		}
+		@Override
+		public void addMultipleNew(long count, UnsafeConsumer<To, IOException> initializer) throws IOException{
+			data.addMultipleNew(count, from->initializer.accept(map(from)));
+		}
+		
+		@Override
+		public void clear() throws IOException{
+			data.clear();
+		}
+		
+		@Override
+		public String toString(){
+			return stream().map(Utils::toShortString).collect(Collectors.joining(", ", "[", "]"));
+		}
+		
+		@Override
+		public Stream<To> stream(){
+			return data.stream().map(this::map);
+		}
+		
+		@Override
+		public Optional<To> first(){
+			return data.first().map(this::map);
+		}
+		
+		@Override
+		public int hashCode(){
+			return data.hashCode();
+		}
+		
+		@Override
+		public boolean equals(Object o){
+			
+			if(this==o){
+				return true;
+			}
+			if(!(o instanceof IOList<?> that)){
+				return false;
+			}
+			
+			var siz=size();
+			if(siz!=that.size()){
+				return false;
+			}
+			
+			var iThis=iterator();
+			var iThat=that.iterator();
+			
+			for(long i=0;i<siz;i++){
+				var vThis=iThis.next();
+				var vThat=iThat.next();
+				
+				if(!vThis.equals(vThat)){
+					return false;
+				}
+			}
+			
+			return true;
+		}
+		
+		@Override
+		public To getUnsafe(long index){
+			return map(data.getUnsafe(index));
+		}
+		
+		@Override
+		public void addAll(Collection<To> values) throws IOException{
+			List<From> mapped=new ArrayList<>(values.size());
+			for(To v : values){
+				mapped.add(unmap(v));
+			}
+			data.addAll(mapped);
+		}
+		
+		@Override
+		public IOIterator.Iter<To> iterator(){
+			return new IOIterator.Iter<>(){
+				private final IOIterator.Iter<From> src=data.iterator();
+				@Override
+				public boolean hasNext(){
+					return src.hasNext();
+				}
+				@Override
+				public To ioNext() throws IOException{
+					return map(src.ioNext());
+				}
+			};
+		}
+		
+		@Override
+		public IOListIterator<To> listIterator(long startIndex){
+			return new IOListIterator<>(){
+				private final IOListIterator<From> src=data.listIterator(startIndex);
+				@Override
+				public boolean hasNext(){
+					return src.hasNext();
+				}
+				@Override
+				public To ioNext() throws IOException{
+					return map(src.ioNext());
+				}
+				@Override
+				public boolean hasPrevious(){
+					return src.hasPrevious();
+				}
+				@Override
+				public To ioPrevious() throws IOException{
+					return map(src.ioPrevious());
+				}
+				@Override
+				public long nextIndex(){
+					return src.nextIndex();
+				}
+				@Override
+				public long previousIndex(){
+					return src.previousIndex();
+				}
+				@Override
+				public void ioRemove() throws IOException{
+					src.ioRemove();
+				}
+				@Override
+				public void ioSet(To to) throws IOException{
+					src.ioSet(unmap(to));
+				}
+				@Override
+				public void ioAdd(To to) throws IOException{
+					src.ioAdd(unmap(to));
+				}
+			};
+		}
+		
+		@Override
+		public void modify(long index, UnsafeFunction<To, To, IOException> modifier) throws IOException{
+			data.modify(index, obj->unmap(modifier.apply(map(obj))));
+		}
+		@Override
+		public boolean isEmpty(){
+			return data.isEmpty();
+		}
+		@Override
+		public To addNew() throws IOException{
+			return map(data.addNew());
+		}
+		@Override
+		public void addMultipleNew(long count) throws IOException{
+			data.addMultipleNew(count);
+		}
+		@Override
+		public void requestCapacity(long capacity) throws IOException{
+			data.requestCapacity(capacity);
+		}
+		@Override
+		public boolean contains(To value) throws IOException{
+			return data.contains(unmap(value));
+		}
+		@Override
+		public long indexOf(To value) throws IOException{
+			return data.indexOf(unmap(value));
+		}
+	}
+	
 	static <T> IOList<T> wrap(List<T> data, Supplier<T> typeConstructor){
 		return new MemoryWrappedIOList<>(data, typeConstructor);
+	}
+	
+	static <From, To> IOList<To> map(IOList<From> data, Function<From, To> map, Function<To, From> unmap){
+		Objects.requireNonNull(data);
+		Objects.requireNonNull(map);
+		Objects.requireNonNull(unmap);
+		return new MappedIOList<From, To>(data){
+			protected To map(From v){return map.apply(v);}
+			protected From unmap(To v){return unmap.apply(v);}
+		};
 	}
 	
 	
