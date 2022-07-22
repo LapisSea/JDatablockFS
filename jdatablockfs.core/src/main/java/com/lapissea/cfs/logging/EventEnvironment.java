8ea0b70b795b715b14366a1f705dfccf8eeb236f
LapisSea <officiallapisseamail@gmail.com>
2022-07-22 09:54:53 +0200

    More work on events, issues with self modificatoin unresolved

--- jdatablockfs.core/src/main/java/com/lapissea/cfs/logging/EventEnvironment.java
+++ jdatablockfs.core/src/main/java/com/lapissea/cfs/logging/EventEnvironment.java
@@ -1,16 +1,22 @@
 package com.lapissea.cfs.logging;
 
+import com.lapissea.cfs.chunk.Cluster;
+import com.lapissea.cfs.io.impl.MemoryData;
+import com.lapissea.cfs.io.instancepipe.ContiguousStructPipe;
+import com.lapissea.cfs.objects.ObjectID;
 import com.lapissea.cfs.type.IOInstance;
+import com.lapissea.cfs.type.Struct;
+import com.lapissea.cfs.type.field.annotations.IODependency;
 import com.lapissea.cfs.type.field.annotations.IONullability;
 import com.lapissea.cfs.type.field.annotations.IOValue;
 
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.function.Function;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
+
+import static com.lapissea.cfs.type.StagedInit.STATE_DONE;
 
 public class EventEnvironment extends IOInstance<EventEnvironment>{
 	
@@ -42,7 +48,7 @@ public class EventEnvironment extends IOInstance<EventEnvironment>{
 			if(refId==NO_ID){
 				if(eventObj==null) throw new IllegalStateException("Reference has no id");
 				var id=eventObj.id;
-				if(id==NO_ID) throw new IllegalStateException("Event was not logged");
+				if(id==NO_ID) throw new IllegalStateException(eventObj+" was not logged");
 				refId=id;
 			}
 			return refId;
@@ -54,50 +60,165 @@ public class EventEnvironment extends IOInstance<EventEnvironment>{
 			}
 			return eventObj;
 		}
+		public String getName(){
+			return name;
+		}
+	}
+	
+	public static class Timeline extends IOInstance<Timeline>{
+		@IOValue
+		@IONullability(IONullability.Mode.NULLABLE)
+		private String name;
+		
+		@IOValue
+		@IODependency("name")
+		private List<Event> events=new ArrayList<>();
+		
+		public Timeline(){}
+		public Timeline(String name){
+			this.name=name;
+		}
+		
+		public String getName(){
+			return name;
+		}
+		public List<Event> getEvents(){
+			return events;
+		}
+		
+		@IOValue
+		public void setEvents(List<Event> events){
+			this.events=events;
+			for(Event e : events){
+				e.timelineName=name;
+			}
+		}
 	}
 	
 	public static class Event extends IOInstance<Event>{
 		
-		@IOValue
-		private long id;
+		private String timelineName;
 		
 		@IOValue
-		private long   timelinePos;
+		@IODependency.VirtualNumSize
+		private long id=NO_ID;
+		
 		@IOValue
-		@IONullability(IONullability.Mode.NULLABLE)
-		private String timelineName;
+		@IODependency.VirtualNumSize
+		private long timelinePos;
+		
 		@IOValue
-		private String name;
+		private String eventName;
 		
 		@IOValue
 		private List<EventReference> references;
 		
-	}
-	
-	
-	@IOValue
-	private long idCounter;
-	
-	@IOValue
-	private       List<Event>           events =new ArrayList<>();
-	private final List<Supplier<Event>> pending=new ArrayList<>();
-	
-	public <T> void log(Function<T, Event> addapter, T value){
-		synchronized(pending){
-			pending.add(()->addapter.apply(value));
+		public Event(){}
+		public Event(long timelinePos, String timelineName, String eventName, List<EventReference> references){
+			this.timelinePos=timelinePos;
+			if(timelinePos<0) throw new IllegalArgumentException(timelinePos+"");
+			this.timelineName=Objects.requireNonNull(timelineName);
+			this.eventName=eventName;
+			this.references=List.copyOf(references);
+		}
+		
+		public String getTimelineName(){
+			return timelineName;
+		}
+		public long getTimelinePos(){
+			return timelinePos;
+		}
+		public String getEventName(){
+			return eventName;
+		}
+		@IOValue
+		public void setEventName(String eventName){
+			this.eventName=eventName;
+		}
+		public List<EventReference> getReferences(){
+			return references;
 		}
 	}
 	
-	private void log(Event event){
+	public class AdaptedLog<T>{
+		private final BiConsumer<Consumer<Event>, T> addapter;
+		public AdaptedLog(BiConsumer<Consumer<Event>, T> addapter){
+			this.addapter=addapter;
+		}
+		
+		private class ResolveEvent implements Supplier<List<Event>>{
+			
+			private T           value;
+			private List<Event> resolved;
+			
+			public ResolveEvent(T value){
+				this.value=value;
+			}
+			
+			@Override
+			public List<Event> get(){
+				resolve();
+				return resolved;
+			}
+			
+			private synchronized void resolve(){
+				if(resolved!=null) return;
+				List<Event> arr=new ArrayList<>();
+				addapter.accept(arr::add, Objects.requireNonNull(value));
+				resolved=List.copyOf(arr);
+				value=null;
+				for(Event event : arr){
+					logEvent(event);
+				}
+			}
+		}
+		
+		
+		public Supplier<List<Event>> log(T value){
+			var r=new ResolveEvent(value);
+			pending.add(r);
+			return r;
+		}
+	}
+	
+	@IOValue
+	private long   idCounter;
+	@IOValue
+	private String name;
+	
+	@IOValue
+	private       List<Timeline>                   timelines=new ArrayList<>();
+	private final List<AdaptedLog<?>.ResolveEvent> pending  =new ArrayList<>();
+	
+	public EventEnvironment(){}
+	public EventEnvironment(String name){
+		this.name=name;
+	}
+	
+	public <T> AdaptedLog<T> addaptLog(BiConsumer<Consumer<Event>, T> addapter){
+		return new AdaptedLog<>(addapter);
+	}
+	
+	
+	private void logEvent(Event event){
 		Objects.requireNonNull(event);
 		
-		synchronized(event){
-			if(event.id!=NO_ID){
-				throw new IllegalArgumentException("Event has already been logged");
-			}
-			event.id=newId();
+		if(event.id!=NO_ID){
+			throw new IllegalArgumentException("Event has already been logged");
 		}
-		events.add(event);
+		event.id=newId();
+		
+		var n=event.timelineName;
+		
+		for(var timeline : timelines){
+			if(timeline.name.equals(n)){
+				timeline.events.add(event);
+				return;
+			}
+		}
+		var t=new Timeline(n);
+		t.events.add(event);
+		timelines.add(t);
 	}
 	
 	private synchronized long newId(){
@@ -106,23 +227,51 @@ public class EventEnvironment extends IOInstance<EventEnvironment>{
 	
 	private Event eventById(long id){
 		if(id==NO_ID) throw new IllegalArgumentException();
-		return events.stream().filter(e->e.id==id).findAny().orElseGet(()->{
-			if(pending.isEmpty())throw new NoSuchElementException();
+		return timelines.stream().flatMap(t->t.events.stream()).filter(e->e.id==id).findAny().orElseGet(()->{
+			if(pending.isEmpty()) throw new NoSuchElementException();
 			flushPending();
 			return eventById(id);
 		});
 	}
 	
-	public void emit(OutputStream dest){
+	public List<Timeline> getTimelines(){
+		return timelines;
+	}
+	
+	
+	public MemoryData<?> emit() throws IOException{
+		preloadTypes();
 		flushPending();
+		var mem    =MemoryData.builder().build();
+		var cluster=Cluster.init(mem);
+		cluster.getRootProvider().provide(this, new ObjectID(name));
+		return mem;
+	}
+	
+	static{
+//		Runner.compileTask(()->{
+//			ContiguousStructPipe.of(EventEnvironment.class);
+//			Struct.of(Event.class);
+//			Struct.of(EventReference.class);
+//			Struct.of(Timeline.class);
+//			Struct.of(Cluster.RootRef.class);
+//		});
+	}
+	
+	private void preloadTypes(){
+		ContiguousStructPipe.of(EventEnvironment.class, STATE_DONE);
+		Struct.of(Event.class, STATE_DONE);
+		Struct.of(EventReference.class, STATE_DONE);
+		Struct.of(Timeline.class, STATE_DONE);
+		Struct.of(Cluster.RootRef.class, STATE_DONE);
 	}
 	
 	
 	private void flushPending(){
-		synchronized(pending){
-			pending.stream().map(Supplier::get).forEach(this::log);
-			pending.clear();
+		for(int i=pending.size()-1;i>=0;i--){
+			pending.remove(i).resolve();
 		}
+		timelines.sort(Comparator.comparing(Timeline::getName));
 	}
 	
 }
--- jdatablockfs.core/src/main/java/com/lapissea/cfs/type/StagedInit.java
+++ jdatablockfs.core/src/main/java/com/lapissea/cfs/type/StagedInit.java
@@ -1,18 +1,21 @@
 package com.lapissea.cfs.type;
 
 import com.lapissea.cfs.internal.Runner;
-import com.lapissea.util.LogUtil;
-import com.lapissea.util.TextUtil;
+import com.lapissea.cfs.logging.EventEnvironment;
 import com.lapissea.util.UtilL;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.Duration;
 import java.time.Instant;
-import java.util.Collections;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
 
 import static com.lapissea.cfs.GlobalConfig.PRINT_COMPILATION;
 
@@ -22,11 +25,12 @@ public abstract class StagedInit{
 	
 	private static final ThreadLocal<Deque<StagedInit>> INIT_STACK=ThreadLocal.withInitial(LinkedList::new);
 	
-	public record StageSnap(StagedInit val, int state){
+	private record StageSnap(StagedInit val, int state, Thread thread, Instant time){
 		
 		static StageSnap of(StagedInit val){
 			if(val==null) return null;
-			return new StageSnap(val, val.getEstimatedState());
+			var now=Instant.now();
+			return new StageSnap(val, val.getEstimatedState(), Thread.currentThread(), now);
 		}
 		
 		@Override
@@ -35,22 +39,47 @@ public abstract class StagedInit{
 		}
 	}
 	
-	public record StageEvent(StageSnap cause, StageSnap subject, Thread thread, Instant time, String note){}
+	private record StageEvent(List<EventEnvironment.EventReference> references, StageSnap subject){}
 	
-	private static final List<StageEvent> STAGE_EVENTS=Collections.synchronizedList(new LinkedList<>());
+	private static final Instant                                 START       =Instant.now();
+	private static final EventEnvironment                        STAGE_EVENTS=new EventEnvironment("Stage events");
+	private static final EventEnvironment.AdaptedLog<StageEvent> ELOG        =STAGE_EVENTS.addaptLog((log, e)->{
+
+//		if(e.cause!=null){
+//			var ref=new EventEnvironment.Event(Duration.between(START, e.cause.time).toNanos(), e.cause.thread.getName(), e.cause.toString(), List.of());
+//			log.accept(ref);
+//			refs=List.of(new EventEnvironment.EventReference(ref, e.note));
+//		}
+		
+		log.accept(new EventEnvironment.Event(Duration.between(START, e.subject.time).toNanos(), e.subject.thread.getName(), e.subject.toString(), e.references));
+	});
 	
 	static{
 		if(PRINT_COMPILATION){
 			Runtime.getRuntime().addShutdownHook(new Thread(()->{
-				var data=List.copyOf(STAGE_EVENTS);
-				LogUtil.println(TextUtil.toTable(data));
+				try{
+					emit();
+				}catch(IOException ex){
+					ex.printStackTrace();
+				}
 			}));
 		}
 	}
 	
-	private static void log(StagedInit cause, StagedInit subject, String note){
-		var now=Instant.now();
-		STAGE_EVENTS.add(new StageEvent(StageSnap.of(cause), StageSnap.of(subject), Thread.currentThread(), now, note));
+	private static void emit() throws IOException{
+		var mem=STAGE_EVENTS.emit();
+		
+		try(var out=new FileOutputStream("events.bin")){
+			mem.transferTo(out);
+		}
+	}
+	
+	private static Supplier<List<EventEnvironment.Event>> log(List<EventEnvironment.EventReference> cause, StagedInit subject){
+		return ELOG.log(new StageEvent(cause, StageSnap.of(subject)));
+	}
+	private static Optional<EventEnvironment.Event> makeEvent(StageSnap snap){
+		if(snap==null) return Optional.empty();
+		return Optional.of(new EventEnvironment.Event(Duration.between(START, snap.time).toNanos(), snap.thread.getName(), snap.toString(), List.of()));
 	}
 	
 	
@@ -73,27 +102,29 @@ public abstract class StagedInit{
 	protected void init(boolean runNow, Runnable init){
 		var cause=getCause();
 		if(runNow){
+			EventEnvironment.Event start=null;
 			try{
 				setInitState(STATE_START);
-				logStart(cause);
+				start=logStart(cause);
 				init.run();
 				setInitState(STATE_DONE);
-				logEnd();
+				logEnd(cause, start);
 			}catch(Throwable e){
-				logFail();
+				logFail(cause, start);
 				throw e;
 			}
 			return;
 		}
 		runBaseStageTask(()->{
+			EventEnvironment.Event start=null;
 			try{
 				setInitState(STATE_START);
-				logStart(cause);
+				start=logStart(cause);
 				init.run();
 				setInitState(STATE_DONE);
-				logEnd();
+				logEnd(cause, start);
 			}catch(Throwable e){
-				logFail();
+				logFail(cause, start);
 				this.e=e;
 				try{
 					lock.lock();
@@ -116,44 +147,56 @@ public abstract class StagedInit{
 	}
 	
 	
-	private static StagedInit getCause(){
+	private static StageSnap getCause(){
 		if(!PRINT_COMPILATION) return null;
 		var s=INIT_STACK.get();
-		return s.isEmpty()?null:s.getLast();
+		return s.isEmpty()?null:StageSnap.of(s.getLast());
 	}
 	
-	private void logFail(){
+	private void logFail(StageSnap cause, EventEnvironment.Event start){
 		if(!PRINT_COMPILATION) return;
 		
-		log(null, this, "FAIL");
+		var ref     =makeEvent(cause).map(e->new EventEnvironment.EventReference(e, "FAIL"));
+		var startRef=new EventEnvironment.EventReference(start, "Start");
+		
+		log(ref.isEmpty()?List.of(startRef):List.of(ref.get(), startRef), this);
 		
 		var s=INIT_STACK.get();
 		if(!s.remove(this)) throw new RuntimeException();
 	}
 	
-	private void logEnd(){
+	private void logEnd(StageSnap cause, EventEnvironment.Event start){
 		if(!PRINT_COMPILATION) return;
 		
-		log(null, this, "Done");
+		var ref     =makeEvent(cause).map(e->new EventEnvironment.EventReference(e, "Done"));
+		var startRef=new EventEnvironment.EventReference(start, "Start");
+		
+		log(ref.isEmpty()?List.of(startRef):List.of(ref.get(), startRef), this);
 		
 		var s=INIT_STACK.get();
 		if(!s.remove(this)) throw new RuntimeException();
 	}
-	private void logStart(StagedInit cause){
-		if(!PRINT_COMPILATION) return;
+	private EventEnvironment.Event logStart(StageSnap cause){
+		if(!PRINT_COMPILATION) return null;
 		
-		log(cause, this, "Start");
+		var ref=makeEvent(cause).map(e->new EventEnvironment.EventReference(e, "Cause"));
+		
+		var es=log(ref.map(List::of).orElse(List.of()), this);
 		
 		var s=INIT_STACK.get();
 		s.addLast(this);
+		
+		var all=es.get();
+		return all.get(all.size()-1);
 	}
 	
 	public final void waitForState(int state){
 		if(this.state<state){
 			waitForState0(state);
 			assert this.state>=state;
+		}else{
+//			log(getCause(), this, "No wait");
 		}
-//		else log(getCause(), this, "No wait");
 		
 		checkErr();
 	}
@@ -199,9 +242,11 @@ public abstract class StagedInit{
 	
 	private void waitForState0(int state){
 		
-		StagedInit cause=getCause();
+		var                                    cause=getCause();
+		Supplier<List<EventEnvironment.Event>> es   =null;
 		if(PRINT_COMPILATION){
-			log(cause, this, "Wait for   "+stateToStr(state));
+			var ref=makeEvent(cause).map(e->new EventEnvironment.EventReference(e, "Wait for "+stateToStr(state)));
+			es=log(ref.map(List::of).orElse(List.of()), this);
 		}
 		
 		while(true){
@@ -209,7 +254,16 @@ public abstract class StagedInit{
 				lock.lock();
 				checkErr();
 				if(this.state>=state){
-					if(PRINT_COMPILATION) log(cause, this, "Waited for "+stateToStr(state));
+					if(PRINT_COMPILATION){
+						List<EventEnvironment.EventReference> refs;
+						if(es==null) refs=List.of();
+						else{
+							var ls  =es.get();
+							var last=ls.get(ls.size()-1);
+							refs=List.of(new EventEnvironment.EventReference(last, "Waited for "+stateToStr(state)));
+						}
+						log(refs, this);
+					}
 					return;
 				}
 				try{
