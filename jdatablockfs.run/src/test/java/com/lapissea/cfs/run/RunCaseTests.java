7d61abeb329d591204e89fba16ed9b0795c9d9b2
LapisSea <officiallapisseamail@gmail.com>
2022-07-24 23:53:19 +0200

    Add run cases as tests

---
+++ jdatablockfs.run/src/test/java/com/lapissea/cfs/run/RunCaseTests.java
@@ -0,0 +1,28 @@
+package com.lapissea.cfs.run;
+
+import com.lapissea.cfs.run.sparseimage.SparseImage;
+import com.lapissea.cfs.run.world.RandomLists;
+import com.lapissea.cfs.run.world.World;
+import com.lapissea.util.ZeroArrays;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class RunCaseTests{
+	
+	@Test
+	void sparseImage() throws IOException{
+		SparseImage.main(ZeroArrays.ZERO_STRING);
+	}
+	
+	@Test
+	void world() throws IOException{
+		World.main(ZeroArrays.ZERO_STRING);
+	}
+	
+	@Test
+	void randomLists() throws IOException{
+		RandomLists.main(ZeroArrays.ZERO_STRING);
+	}
+	
+}
