85a307d2b14fa02979937b3d6cdcb17c4711bdcd
LapisSea <officiallapisseamail@gmail.com>
2022-10-03 10:40:45 +0200

    Cleanup

--- jdatablockfs.core/src/main/java/com/lapissea/cfs/exceptions/FieldIsNullException.java
+++ jdatablockfs.core/src/main/java/com/lapissea/cfs/exceptions/FieldIsNullException.java
@@ -1,25 +1,48 @@
 package com.lapissea.cfs.exceptions;
 
+import com.lapissea.cfs.Utils;
 import com.lapissea.cfs.type.field.IOField;
+
+import java.util.function.Supplier;
 
 public class FieldIsNullException extends NullPointerException{
 	
 	public static <T> T requireNonNull(IOField<?, ?> field, T obj){
 		if(obj==null){
-			throw new FieldIsNullException(field, (field.declaringStruct()!=null?field.declaringStruct().getType().getName()+".":"")+field.toShortString()+" should not be null");
+			throw new FieldIsNullException(
+				field,
+				()->(field.declaringStruct()!=null?Utils.typeToHuman(field.declaringStruct().getType(), false)+".":"")+
+				    field.toShortString()+
+				    " should not be null"
+			);
 		}
 		return obj;
 	}
 	
 	
+	private final Supplier<String> msgMake;
+	private       String           msg;
+	
 	public final IOField<?, ?> field;
 	
 	public FieldIsNullException(IOField<?, ?> field){
-		super(field+" is null");
+		this.field=field;
+		msgMake=()->Utils.typeToHuman(this.field.getClass(), false)+" - "+this.field+" is null";
+	}
+	
+	public FieldIsNullException(IOField<?, ?> field, Supplier<String> msgMake){
+		this.msgMake=msgMake;
 		this.field=field;
 	}
-	public FieldIsNullException(IOField<?, ?> field, String s){
-		super(s);
-		this.field=field;
+	
+	@Override
+	public String getLocalizedMessage(){
+		return getMessage();
+	}
+	
+	@Override
+	public String getMessage(){
+		if(msg==null) msg=msgMake.get();
+		return msg;
 	}
 }
