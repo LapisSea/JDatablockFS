a8ec117972e255d794d3bd54c8c49cbc9fcc6cb7
LapisSea <officiallapisseamail@gmail.com>
2022-11-13 18:34:15 +0100

    QueryExpressionParser: add support for in string numbers and clenaup

--- jdatablockfs.core/src/main/java/com/lapissea/cfs/query/QueryExpressionParser.java
+++ jdatablockfs.core/src/main/java/com/lapissea/cfs/query/QueryExpressionParser.java
@@ -3,7 +3,6 @@ package com.lapissea.cfs.query;
 import com.lapissea.cfs.type.IOInstance;
 import com.lapissea.cfs.type.Struct;
 import com.lapissea.cfs.type.field.IOField;
-import com.lapissea.util.LogUtil;
 import com.lapissea.util.NotImplementedException;
 import com.lapissea.util.UtilL;
 
@@ -15,17 +14,36 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Long.parseLong;
 
 public class QueryExpressionParser{
 	
 	private sealed interface ArgSource{
-		record Root() implements ArgSource{}
+		record Root() implements ArgSource{
+			@Override
+			public String toString(){
+				return "args";
+			}
+		}
 		
-		record GetArray(int index, ArgSource source) implements ArgSource{}
+		record GetArray(int index, ArgSource source) implements ArgSource{
+			@Override
+			public String toString(){
+				return source+"["+index+"]";
+			}
+		}
 		
-		record Litelar(Object value) implements ArgSource{}
+		record Litelar(Object value) implements ArgSource{
+			@Override
+			public String toString(){
+				return value+"";
+			}
+		}
 	}
 	
 	private interface ArgContain{
@@ -39,35 +57,45 @@ public class QueryExpressionParser{
 	private sealed interface Check{
 		record And(Check l, Check r) implements Check{
 			@Override
-			public Stream<Check> deep(){return Stream.of(Stream.of(this), l.deep(), r.deep()).flatMap(s->s);}
+			public String toString(){
+				return "("+l+" && "+r+")";
+			}
 		}
 		
 		record Or(Check l, Check r) implements Check{
 			@Override
-			public Stream<Check> deep(){return Stream.of(Stream.of(this), l.deep(), r.deep()).flatMap(s->s);}
+			public String toString(){
+				return "("+l+" || "+r+")";
+			}
 		}
 		
 		record Not(Check check) implements Check{
 			@Override
-			public Stream<Check> deep(){return Stream.of(Stream.of(this), check.deep()).flatMap(s->s);}
+			public String toString(){
+				return "!"+check;
+			}
 		}
 		
 		record Equals(String name, ArgSource arg) implements Check, ArgContain, FieldRef{
 			@Override
-			public Stream<Check> deep(){return Stream.of(this);}
+			public String toString(){
+				return name+" == "+arg;
+			}
 		}
 		
 		record GreaterThan(String name, ArgSource arg) implements Check, ArgContain, FieldRef{
 			@Override
-			public Stream<Check> deep(){return Stream.of(this);}
+			public String toString(){
+				return name+" > "+arg;
+			}
 		}
 		
 		record LessThan(String name, ArgSource arg) implements Check, ArgContain, FieldRef{
 			@Override
-			public Stream<Check> deep(){return Stream.of(this);}
+			public String toString(){
+				return name+" < "+arg;
+			}
 		}
-		
-		Stream<Check> deep();
 	}
 	
 	private record FilterQuery<T>(Class<T> type, String expression, int hash){
@@ -93,8 +121,8 @@ public class QueryExpressionParser{
 	private static <T> FilterResult<T> parse(FilterQuery<T> filterQuery){
 		Check check=expressionToCheck(filterQuery.expression);
 		
-		var args  =check.deep().filter(c->c instanceof ArgContain).map(c->((ArgContain)c).arg()).collect(Collectors.toSet());
-		var fields=check.deep().filter(c->c instanceof FieldRef).map(c->((FieldRef)c).name()).collect(Collectors.toUnmodifiableSet());
+		var args  =deep(check).filter(c->c instanceof ArgContain).map(c->((ArgContain)c).arg()).collect(Collectors.toSet());
+		var fields=deep(check).filter(c->c instanceof FieldRef).map(c->((FieldRef)c).name()).collect(Collectors.toUnmodifiableSet());
 		
 		Consumer<T>              argCheck =generateArgCheck(args);
 		BiPredicate<Object[], T> predicate=generateFilter(args, check);
@@ -136,11 +164,23 @@ public class QueryExpressionParser{
 			case Check.Or or -> reflectionCheck(args, obj, or.l)||reflectionCheck(args, obj, or.r);
 		};
 	}
+	
 	private static Object getArg(Object[] args, ArgSource arg){
 		return switch(arg){
 			case ArgSource.GetArray getArray -> Array.get(getArg(args, getArray.source), getArray.index);
 			case ArgSource.Root root -> args;
 			case ArgSource.Litelar litelar -> litelar.value;
+		};
+	}
+	
+	private static Stream<Check> deep(Check check){
+		return switch(check){
+			case Check.And c -> Stream.of(Stream.of(c), deep(c.l), deep(c.r)).flatMap(s->s);
+			case Check.Or c -> Stream.of(Stream.of(c), deep(c.l), deep(c.r)).flatMap(s->s);
+			case Check.Equals c -> Stream.of(c);
+			case Check.GreaterThan c -> Stream.of(c);
+			case Check.LessThan c -> Stream.of(c);
+			case Check.Not c -> Stream.of(Stream.of(c), deep(c.check)).flatMap(s->s);
 		};
 	}
 	
@@ -185,13 +225,15 @@ public class QueryExpressionParser{
 			skipWhite();
 			if(str.charAt(pos)!='{') return null;
 			pos++;
-			return advance((i, c)->c!='}');
+			var inside=advance((i, c)->c!='}');
+			pos++;
+			return inside;
 		}
+		
 		public String brace(){
 			skipWhite();
 			if(str.charAt(pos)!='(') return null;
 			pos++;
-			
 			
 			int[] depth={1};
 			return advance((i, c)->{
@@ -200,30 +242,71 @@ public class QueryExpressionParser{
 				return depth[0]>0;
 			});
 		}
-		private boolean and(){
-			return match(List.of("&", "&&", "and"));
+		
+		public Number number(){
+			var hex=regex(Pattern.compile("^[-+]?0[xX][0-9a-fA-F_]+"));
+			if(hex!=null){
+				hex=cleanNumber(hex, "0x");
+				return parseLong(hex, 16);
+			}
+			var bin=regex(Pattern.compile("^[-+]??0[bB][0-1_]+"));
+			if(bin!=null){
+				bin=cleanNumber(bin, "0b");
+				return parseLong(bin.replace("_", ""), 2);
+			}
+			var dec=regex(Pattern.compile("^[-+]??[0-9_]*\\.?[0-9_]*"));
+			if(dec!=null){
+				dec=cleanNumber(dec, null);
+				if(dec.contains(".")){
+					return parseDouble(dec.replace("_", ""));
+				}
+				return parseLong(dec);
+			}
+			
+			return null;
 		}
-		private boolean or(){
-			return match(List.of("|", "||", "or"));
+		
+		private static String cleanNumber(String num, String prefix){
+			if(num.charAt(0)=='+') num=num.substring(1);
+			if(prefix!=null){
+				if(num.charAt(0)=='-'){
+					num="-"+num.substring(prefix.length()+1);
+				}else{
+					num=num.substring(prefix.length());
+				}
+			}
+			num=num.replace("_", "");
+			return num;
 		}
-		private boolean not(){
-			return match(List.of("!", "not"));
+		
+		private String regex(Pattern pattern){
+			skipWhite();
+			var matcher=pattern.matcher(str.subSequence(pos, str.length()));
+			if(!matcher.find()) return null;
+			var result=matcher.group();
+			if(result.isEmpty()) return null;
+			pos+=result.length();
+			return result;
 		}
-		private boolean equals(){
-			return exact("==")||exact("=")||match(List.of("equals"));
-		}
-		private boolean greater(){
-			return exact(">");
-		}
-		private boolean lesser(){
-			return exact("<");
-		}
-		private boolean match(List<String> matches){
+		
+		private boolean and()    {return match("&", "&&", "and");}
+		private boolean or()     {return match("||", "|", "or");}
+		private boolean not()    {return match("!", "not");}
+		private boolean equals() {return match("==", "=", "is", "equals");}
+		private boolean greater(){return match(">");}
+		private boolean lesser() {return match("<");}
+		private boolean in()     {return match("in");}
+		
+		private boolean match(String... matches){
 			skipWhite();
 			for(var match : matches){
-				if(str.substring(pos).toLowerCase().startsWith(match)){
-					var c=str.charAt(pos+match.length());
-					if(List.of('(', ')', '!', '&', '|').contains(c)||Character.isWhitespace(c)){
+				if(startsWith(match)){
+					boolean validEnd;
+					if(Character.isLetter(match.charAt(match.length()-1))){
+						var c=str.charAt(pos+match.length());
+						validEnd=Character.isWhitespace(c)||List.of('(', ')', '[', ']', '{', '}', '<', '>', '!', '?', '&', '|', '=').contains(c);
+					}else validEnd=true;
+					if(validEnd){
 						pos+=match.length();
 						return true;
 					}
@@ -231,14 +314,11 @@ public class QueryExpressionParser{
 			}
 			return false;
 		}
-		private boolean exact(String match){
-			skipWhite();
-			if(str.substring(pos).toLowerCase().startsWith(match)){
-				pos+=match.length();
-				return true;
-			}
-			return false;
+		
+		private boolean startsWith(String match){
+			return str.regionMatches(true, pos, match, 0, match.length());
 		}
+		
 		private void skipWhite(){
 			for(int i=pos;i<str.length();i++){
 				if(Character.isWhitespace(str.charAt(i))) pos++;
@@ -258,8 +338,8 @@ public class QueryExpressionParser{
 		Check parse(){
 			Check check=null;
 			while(true){
-				if(reader.not()){
-					notNext=!notNext;
+				if(!notNext&&reader.not()){
+					notNext=true;
 					continue;
 				}
 				
@@ -277,6 +357,11 @@ public class QueryExpressionParser{
 				}else{
 					check=requireNextCheck();
 					continue;
+				}
+				
+				reader.skipWhite();
+				if(reader.str.length()!=reader.pos){
+					throw new RuntimeException("Unexpected end. Remaining: \""+reader.str.substring(reader.pos)+"\"");
 				}
 				
 				return check;
@@ -304,34 +389,38 @@ public class QueryExpressionParser{
 			
 			var fieldName=reader.field();
 			if(fieldName!=null){
+				
 				if(reader.equals()){
-					var argStr=reader.squiggly();
-					if(argStr!=null){
-						int index=Integer.parseInt(argStr);
-						return new Check.Equals(fieldName, new ArgSource.GetArray(index, new ArgSource.Root()));
-					}
-					throw new NotImplementedException();
+					var source=source();
+					return new Check.Equals(fieldName, source);
 				}
 				if(reader.greater()){
-					var argStr=reader.squiggly();
-					if(argStr!=null){
-						int index=Integer.parseInt(argStr);
-						return new Check.GreaterThan(fieldName, new ArgSource.GetArray(index, new ArgSource.Root()));
-					}
-					throw new NotImplementedException();
+					var source=source();
+					return new Check.GreaterThan(fieldName, source);
 				}
 				if(reader.lesser()){
-					var argStr=reader.squiggly();
-					if(argStr!=null){
-						int index=Integer.parseInt(argStr);
-						return new Check.LessThan(fieldName, new ArgSource.GetArray(index, new ArgSource.Root()));
-					}
-					throw new NotImplementedException();
+					var source=source();
+					return new Check.LessThan(fieldName, source);
 				}
-				LogUtil.println(fieldName);
+				
+				throw new RuntimeException("Unexpected syntax. Expected == or > or <");
 			}
 			
 			throw new NotImplementedException();
+		}
+		
+		private ArgSource source(){
+			var argStr=reader.squiggly();
+			if(argStr!=null){
+				int index=Integer.parseInt(argStr);
+				return new ArgSource.GetArray(index, new ArgSource.Root());
+			}
+			var num=reader.number();
+			if(num!=null){
+				return new ArgSource.Litelar(num);
+			}
+			
+			throw new RuntimeException("???");
 		}
 	}
 	
