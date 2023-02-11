5805b0a2bf0a0bafe16ea39ce681a8226b7878aa
LapisSea <officiallapisseamail@gmail.com>
2023-02-11 16:42:37 +0100

    Refractor RandomLists run case

--- jdatablockfs.run/src/main/java/com/lapissea/cfs/run/world/RandomLists.java
+++ jdatablockfs.run/src/main/java/com/lapissea/cfs/run/world/RandomLists.java
@@ -1,10 +1,12 @@
 package com.lapissea.cfs.run.world;
 
 import com.lapissea.cfs.chunk.Cluster;
+import com.lapissea.cfs.io.impl.MemoryData;
 import com.lapissea.cfs.logging.Log;
 import com.lapissea.cfs.run.Configuration;
+import com.lapissea.cfs.tools.logging.DataLogger;
 import com.lapissea.cfs.tools.logging.LoggedMemoryUtils;
-import com.lapissea.util.ConsoleColors;
+import com.lapissea.util.LateInit;
 import com.lapissea.util.LogUtil;
 import com.lapissea.util.UtilL;
 
@@ -24,36 +26,15 @@ public class RandomLists{
 	}
 	
 	public static void main(Configuration.View conf){
-		IntStream.range(conf.getInt("min", 1), conf.getInt("max", 30)).mapToObj(listCount -> {
+		IntStream.range(conf.getInt("min", 1), conf.getInt("max", 20)).mapToObj(listCount -> {
 			try{
 				var logger = LoggedMemoryUtils.createLoggerFromConfig();
 				var mem    = LoggedMemoryUtils.newLoggedMemory("l" + listCount, logger);
 				logger.get().getSession("l" + listCount);
 				
 				var task = CompletableFuture.runAsync(() -> {
-					Log.info(ConsoleColors.PURPLE + "Starting: {} lists" + ConsoleColors.RESET, listCount);
-					try{
-						var rand = new Random((long)listCount<<4);
-						try{
-							var cl = Cluster.init(mem);
-							var p  = cl.getRootProvider();
-							for(int i = 0; i<400; i++){
-								var m = p.request("map" + rand.nextInt(listCount), Map.class);
-								m.entities.addNew(e -> {
-									e.inventory.add(new InventorySlot());
-								});
-							}
-							cl.defragment();
-						}finally{
-							logger.block();
-							mem.getHook().writeEvent(mem, LongStream.of());
-						}
-					}catch(Throwable e){
-						throw new RuntimeException(e);
-					}finally{
-						logger.get().destroy();
-					}
-				}, Thread.ofVirtual()::start);
+					task(listCount, logger, mem);
+				});
 				if(conf.getBoolean("async", true)) UtilL.sleep(10);
 				else task.join();
 				return task;
@@ -62,7 +43,31 @@ public class RandomLists{
 				throw new RuntimeException(e);
 			}
 		}).toList().forEach(CompletableFuture::join);
-		
+	}
+	
+	private static void task(int listCount, LateInit.Safe<DataLogger> logger, MemoryData<?> mem){
+		Log.info("{#purpleStarting: {} lists#}", listCount);
+		try{
+			var rand = new Random((long)listCount<<4);
+			try{
+				var cl = Cluster.init(mem);
+				var p  = cl.getRootProvider();
+				for(int i = 0; i<400; i++){
+					var m = p.request("map" + rand.nextInt(listCount), Map.class);
+					m.entities.addNew(e -> {
+						e.inventory.add(new InventorySlot());
+					});
+				}
+				cl.defragment();
+			}finally{
+				logger.block();
+				mem.getHook().writeEvent(mem, LongStream.of());
+			}
+		}catch(Throwable e){
+			throw new RuntimeException(e);
+		}finally{
+			logger.get().destroy();
+		}
 	}
 	
 }
