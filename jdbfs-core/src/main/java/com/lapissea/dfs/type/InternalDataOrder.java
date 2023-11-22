cd55e574e4c732acdd53781194e1059d2ba615c2
LapisSea <officiallapisseamail@gmail.com>
2023-11-22 10:50:40 +0100

    Clarify InternalDataOrder

--- jdbfs-core/src/main/java/com/lapissea/dfs/type/InternalDataOrder.java
+++ jdbfs-core/src/main/java/com/lapissea/dfs/type/InternalDataOrder.java
@@ -12,6 +12,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
-public @interface DataOrder{
+@Deprecated
+public @interface InternalDataOrder{
 	String[] value();
 }
