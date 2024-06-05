e5cccbabe3ddd7f4cc8a1a47ab0094da76b30314
LapisSea <officiallapisseamail@gmail.com>
2024-06-05 20:34:33 +0200

    Increase precession of logging

--- Fuzzer/src/main/java/com/lapissea/fuzz/FuzzConfig.java
+++ Fuzzer/src/main/java/com/lapissea/fuzz/FuzzConfig.java
@@ -3,6 +3,8 @@ package com.lapissea.fuzz;
 import java.time.Duration;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.function.Consumer;
 
 @SuppressWarnings("unused")
@@ -12,9 +14,9 @@ public record FuzzConfig(
 	Duration errorDelay, Optional<FailOrder> failOrder
 ){
 	public record LogState(
-		float progress,
+		double progress,
 		Duration estimatedTotalTime, Duration estimatedTimeRemaining,
-		Duration elapsed, Duration durationPerOp,
+		Duration elapsed, OptionalDouble nanosecondsPerOp,
 		boolean hasFail
 	){
 		public static String stdTime(Duration tim){
@@ -25,6 +27,11 @@ public record FuzzConfig(
 			                     tim.toSecondsPart());
 		}
 	}
+	
+	static <T> Optional<T> none()   { return Optional.empty(); }
+	static <T> Optional<T> some(T t){ return Optional.of(t); }
+	static OptionalInt some(int i)  { return OptionalInt.of(i); }
+	
 	public FuzzConfig{
 		Objects.requireNonNull(logFunct);
 		if(logTimeout.isNegative()) throw new IllegalArgumentException("logTimeout must be positive");
@@ -34,17 +41,17 @@ public record FuzzConfig(
 	}
 	public FuzzConfig(){
 		this(
-			true, Optional.empty(), Duration.ofMillis(900),
-			Optional.empty(), Math.max(1, Runtime.getRuntime().availableProcessors() + 1),
-			Duration.ofSeconds(2), Optional.empty()
+			true, none(), Duration.ofMillis(900),
+			none(), Math.max(1, Runtime.getRuntime().availableProcessors() + 1),
+			Duration.ofSeconds(2), none()
 		);
 	}
 	
-	public FuzzConfig dontLog()                           { return new FuzzConfig(false, Optional.empty(), logTimeout, name, maxWorkers, errorDelay, failOrder); }
-	public FuzzConfig logWith(Consumer<LogState> logFunct){ return new FuzzConfig(true, Optional.of(logFunct), logTimeout, name, maxWorkers, errorDelay, failOrder); }
+	public FuzzConfig dontLog()                           { return new FuzzConfig(false, none(), logTimeout, name, maxWorkers, errorDelay, failOrder); }
+	public FuzzConfig logWith(Consumer<LogState> logFunct){ return new FuzzConfig(true, some(logFunct), logTimeout, name, maxWorkers, errorDelay, failOrder); }
 	public FuzzConfig withLogTimeout(Duration logTimeout) { return new FuzzConfig(shouldLog, logFunct, logTimeout, name, maxWorkers, errorDelay, failOrder); }
-	public FuzzConfig withName(String name)               { return new FuzzConfig(shouldLog, logFunct, logTimeout, Optional.of(name), maxWorkers, errorDelay, failOrder); }
+	public FuzzConfig withName(String name)               { return new FuzzConfig(shouldLog, logFunct, logTimeout, some(name), maxWorkers, errorDelay, failOrder); }
 	public FuzzConfig withMaxWorkers(int maxWorkers)      { return new FuzzConfig(shouldLog, logFunct, logTimeout, name, maxWorkers, errorDelay, failOrder); }
 	public FuzzConfig withErrorDelay(Duration errorDelay) { return new FuzzConfig(shouldLog, logFunct, logTimeout, name, maxWorkers, errorDelay, failOrder); }
-	public FuzzConfig withFailOrder(FailOrder failOrder)  { return new FuzzConfig(shouldLog, logFunct, logTimeout, name, maxWorkers, errorDelay, Optional.of(failOrder)); }
+	public FuzzConfig withFailOrder(FailOrder failOrder)  { return new FuzzConfig(shouldLog, logFunct, logTimeout, name, maxWorkers, errorDelay, some(failOrder)); }
 }
--- Fuzzer/src/main/java/com/lapissea/fuzz/FuzzProgress.java
+++ Fuzzer/src/main/java/com/lapissea/fuzz/FuzzProgress.java
@@ -1,7 +1,11 @@
 package com.lapissea.fuzz;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.OptionalDouble;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 
@@ -58,12 +62,13 @@ public final class FuzzProgress{
 	}
 	
 	private void logInc(long count, int progressI){
-		if(progressI == last) return;
 		var now = Instant.now();
 		if(Duration.between(lastLog, now).compareTo(config.logTimeout())>0){
 			lastLog = now;
 			last = progressI;
-			log(count, (float)(progressI/1000D));
+			
+			BigDecimal progress = BigDecimal.valueOf(count).divide(BigDecimal.valueOf(totalIterations), MathContext.DECIMAL128);
+			log(count, progress);
 		}
 	}
 	
@@ -75,32 +80,71 @@ public final class FuzzProgress{
 		final String reset = "\033[0m";
 		
 		var f = String.format("%.1f", state.progress()*100);
+		if(f.equals("100.0")) f = "Done";
+		
+		var    perOpO = state.nanosecondsPerOp();
+		String unit;
+		String value;
+		if(perOpO.isEmpty()){
+			unit = "ms";
+			value = "--";
+		}else{
+			var perOp = perOpO.getAsDouble();
+			if(perOp>1000_000_000){
+				unit = " S";
+				value = String.format("~%.4f", perOp/1000_000_000D);
+			}else if(perOp>1000){
+				unit = "ms";
+				value = String.format("~%.4f", perOp/1_000_000D);
+			}else{
+				unit = "Ns";
+				value = String.format("~%.2f", perOp);
+			}
+		}
+		
 		System.out.println(
 			(state.hasFail()? red + "FAIL " : green + "OK | ") +
 			gray + "Progress: " + reset + (f.length()<4? " " + f : f) + "%" +
 			gray + ", ET: " + reset + stdTime(state.estimatedTotalTime()) +
 			gray + ", ETR: " + reset + stdTime(state.estimatedTimeRemaining()) +
 			gray + ", elapsed: " + reset + stdTime(state.elapsed()) +
-			gray + ", ms/op: " + reset + (state.durationPerOp() == null? "--" :
-			                              "~" + String.format("%.3f", state.durationPerOp().toNanos()/1000_000D))
+			gray + ", " + unit + "/op: " + reset + value
 		);
 	};
 	
-	private void log(long count, float progress){
+	private static final BigInteger NANOS_PER_SECOND = BigInteger.valueOf(1000_000_000L);
+	private static BigInteger durationToBigDecimal(Duration duration){
+		var seconds = duration.getSeconds();
+		var nanos   = duration.getNano();
+		return BigInteger.valueOf(seconds).multiply(NANOS_PER_SECOND).add(BigInteger.valueOf(nanos));
+	}
+	private static Duration bigToDuration(BigDecimal val){
+		return val == null? null : bigToDuration(val.toBigInteger());
+	}
+	private static Duration bigToDuration(BigInteger val){
+		if(val == null) return null;
+		var seconds = val.divide(NANOS_PER_SECOND);
+		var nanos   = val.subtract(seconds.multiply(NANOS_PER_SECOND));
+		return Duration.ofSeconds(seconds.longValueExact(), nanos.longValueExact());
+	}
+	
+	private void log(long count, BigDecimal progress){
 		var now = Instant.now();
 		
-		var elapsedTime   = Duration.between(start, now);
-		var elapsedTimeMs = elapsedTime.toMillis();
+		var elapsedTime = Duration.between(start, now);
 		
-		var totalTimeEstimated = elapsedTimeMs/progress;
-		var timeRemaining      = totalTimeEstimated - elapsedTimeMs;
+		var progressZero = progress.equals(BigDecimal.ZERO);
+		
+		var elapsedTimeNs      = new BigDecimal(durationToBigDecimal(elapsedTime));
+		var totalTimeEstimated = progressZero? null : elapsedTimeNs.divide(progress, MathContext.DECIMAL32);
+		var timeRemaining      = progressZero? null : totalTimeEstimated.subtract(elapsedTimeNs);
 		
 		config.logFunct().orElse(TO_CONSOLE).accept(new FuzzConfig.LogState(
-			progress,
-			count<10? null : Duration.ofMillis((long)totalTimeEstimated),
-			count<10? null : Duration.ofMillis((long)timeRemaining),
+			progress.doubleValue(),
+			count<100? null : bigToDuration(totalTimeEstimated),
+			count<100? null : bigToDuration(timeRemaining),
 			elapsedTime,
-			count<10? null : elapsedTime.dividedBy(count),
+			count<100? OptionalDouble.empty() : OptionalDouble.of(elapsedTimeNs.divide(BigDecimal.valueOf(count), MathContext.DECIMAL64).doubleValue()),
 			errMark
 		));
 	}
