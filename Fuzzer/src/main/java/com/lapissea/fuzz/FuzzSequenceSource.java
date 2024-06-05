e32f21f0dfea81aa3968ebf8c163506b8992cfab
LapisSea <officiallapisseamail@gmail.com>
2024-06-05 20:37:35 +0200

    Make FuzzSequenceSource.LenSeed parallel and error free with large numbers

--- Fuzzer/src/main/java/com/lapissea/fuzz/FuzzSequenceSource.java
+++ Fuzzer/src/main/java/com/lapissea/fuzz/FuzzSequenceSource.java
@@ -1,6 +1,7 @@
 package com.lapissea.fuzz;
 
-import java.util.stream.IntStream;
+import java.math.BigInteger;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public interface FuzzSequenceSource{
@@ -15,15 +16,36 @@ public interface FuzzSequenceSource{
 				throw new IllegalArgumentException("Sequence length must be greater than zero");
 			}
 		}
+		
+		private static long mixMurmur64(long z){
+			z = (z^(z >>> 33))*0xff51afd7ed558ccdL;
+			z = (z^(z >>> 33))*0xc4ceb9fe1a85ec53L;
+			return z^(z >>> 33);
+		}
+		
 		@Override
 		public Stream<FuzzSequence> all(){
-			var sequenceEntropy   = new SimpleRandom(seed);
-			var numberOfSequences = Math.toIntExact(Math.ceilDiv(totalIterations, sequenceLength));
-			return IntStream.range(0, numberOfSequences).mapToObj(idx -> {
-				var from = idx*sequenceLength;
-				var to   = Math.min((idx + 1L)*sequenceLength, totalIterations);
-				return new FuzzSequence(from, idx, sequenceEntropy.nextLong(), (int)(to - from));
-			});
+			long numberOfSequences = Math.ceilDiv(totalIterations, sequenceLength);
+			
+			long maxFastIDX       = Long.MAX_VALUE/sequenceLength - 2;
+			var  sequenceLengthL  = BigInteger.valueOf(sequenceLength);
+			var  totalIterationsL = BigInteger.valueOf(totalIterations);
+			
+			var seedBase = mixMurmur64(seed);
+			
+			return LongStream.range(0, numberOfSequences).mapToObj(idx -> {
+				long from = idx*(long)sequenceLength;
+				long to;
+				if(idx>=maxFastIDX){
+					var unbound = BigInteger.valueOf(idx).add(BigInteger.ONE).multiply(sequenceLengthL);
+					to = unbound.min(totalIterationsL).longValueExact();
+				}else{
+					var unbound = (idx + 1L)*sequenceLength;
+					to = Math.min(totalIterations, unbound);
+				}
+				
+				return new FuzzSequence(from, idx, seedBase^mixMurmur64(idx), (int)(to - from));
+			}).parallel();
 		}
 	}
 }
