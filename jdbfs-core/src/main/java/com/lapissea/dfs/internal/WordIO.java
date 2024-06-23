9292a2e49e0a7b291a74aad341af107538f5914b
LapisSea <officiallapisseamail@gmail.com>
2024-06-23 22:36:44 +0200

    Optimize word IO. Make use of unsafe if enabled

--- jdbfs-core/src/main/java/com/lapissea/dfs/internal/WordIO.java
+++ jdbfs-core/src/main/java/com/lapissea/dfs/internal/WordIO.java
@@ -2,72 +2,39 @@ package com.lapissea.dfs.internal;
 
 import com.lapissea.dfs.io.content.BBView;
 
-import java.nio.ByteOrder;
-
 public final class WordIO{
-	private static final boolean IS_LITTLE_ENDIAN = ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN;
 	
 	public static long getWord(byte[] data, int off, int len){
-		if(IS_LITTLE_ENDIAN) return getWordLE(data, off, len);
-		return getWordLoopBE(data, off, len);
-	}
-	
-	public static void setWord(long v, byte[] writeBuffer, int off, int len){
-		if(IS_LITTLE_ENDIAN) setWordLE(v, writeBuffer, off, len);
-		else setWordLoopBE(v, writeBuffer, off, len);
-	}
-	
-	private static long getWordLE(byte[] data, int off, int len){
 		return switch(len){
+			case 0 -> 0;
 			case 1 -> Byte.toUnsignedLong(data[off]);
 			case 2 -> BBView.readUnsignedInt2(data, off);
 			case 3 -> BBView.readUnsignedInt3(data, off);
 			case 4 -> Integer.toUnsignedLong(BBView.readInt4(data, off));
+			case 5 -> BBView.readUnsignedInt5(data, off);
+			case 6 -> BBView.readUnsignedInt6(data, off);
+			case 7 -> BBView.readUnsignedInt7(data, off);
 			case 8 -> BBView.readInt8(data, off);
-			default -> getWordLoopLE(data, off, len);
+			default -> throw fail(len);
 		};
 	}
 	
-	private static void setWordLE(long v, byte[] writeBuffer, int off, int len){
-		if(v == 0){
-			for(int i = off, j = off + len; i<j; i++){
-				writeBuffer[i] = 0;
-			}
-			return;
-		}
+	public static void setWord(long v, byte[] writeBuffer, int off, int len){
 		switch(len){
+			case 0 -> { }
 			case 1 -> writeBuffer[off] = (byte)v;
 			case 2 -> BBView.writeInt2(writeBuffer, off, (short)v);
 			case 3 -> BBView.writeInt3(writeBuffer, off, (int)v);
 			case 4 -> BBView.writeInt4(writeBuffer, off, (int)v);
+			case 5 -> BBView.writeInt5(writeBuffer, off, v);
+			case 6 -> BBView.writeInt6(writeBuffer, off, v);
+			case 7 -> BBView.writeInt7(writeBuffer, off, v);
 			case 8 -> BBView.writeInt8(writeBuffer, off, v);
-			default -> setWordLoopLE(v, writeBuffer, off, len);
+			default -> throw fail(len);
 		}
 	}
 	
-	private static long getWordLoopLE(byte[] data, int off, int len){
-		long val = 0;
-		for(int i = 0; i<len; i++){
-			val |= (data[off + i]&255L)<<(i*8);
-		}
-		return val;
-	}
-	private static void setWordLoopLE(long v, byte[] writeBuffer, int off, int len){
-		for(int i = 0; i<len; i++){
-			writeBuffer[off + i] = (byte)(v >>> (i*8));
-		}
-	}
-	
-	private static long getWordLoopBE(byte[] data, int off, int len){
-		long val = 0;
-		for(int i = 0; i<len; i++){
-			val |= (data[off + i]&255L)<<(i*8);
-		}
-		return val;
-	}
-	private static void setWordLoopBE(long v, byte[] writeBuffer, int off, int len){
-		for(int i = 0; i<len; i++){
-			writeBuffer[off + i] = (byte)(v >>> (i*8));
-		}
+	private static RuntimeException fail(int len){
+		throw new IllegalArgumentException("The length of a word must be in the range of 0-8" + len);
 	}
 }
--- jdbfs-core/src/main/java/com/lapissea/dfs/io/content/BBView.java
+++ jdbfs-core/src/main/java/com/lapissea/dfs/io/content/BBView.java
@@ -1,12 +1,137 @@
 package com.lapissea.dfs.io.content;
 
+import com.lapissea.dfs.config.ConfigDefs;
+import com.lapissea.dfs.internal.MyUnsafe;
+import com.lapissea.dfs.type.compilation.FieldCompiler;
+
 import java.lang.invoke.VarHandle;
+import java.util.Objects;
 
 import static java.lang.invoke.MethodHandles.byteArrayViewVarHandle;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
 
-@SuppressWarnings({"PointlessArithmeticExpression", "PointlessBitwiseExpression"})
 public final class BBView{
+	
+	private static final class UnsafeView{
+		
+		private static final int ARRAY_BYTE_BASE_OFFSET = MyUnsafe.UNSAFE.arrayBaseOffset(byte[].class);
+		
+		private static long mem(byte[] ba, int index, int size){
+			return Objects.checkIndex(index, ba.length - (size - 1)) + ARRAY_BYTE_BASE_OFFSET;
+		}
+		
+		private static void putByte_(byte[] byteBuffer, long memOff, int localOff, long v){
+			MyUnsafe.UNSAFE.putByte(byteBuffer, memOff + localOff, (byte)(v >>> (localOff*8)));
+		}
+		private static int getByte_(byte[] byteBuffer, long memOff){
+			return MyUnsafe.UNSAFE.getByte(byteBuffer, memOff)&0xFF;
+		}
+		private static long getByte_(byte[] byteBuffer, long memOff, int localOff){
+			return ((long)getByte_(byteBuffer, memOff + localOff))<<(localOff*8);
+		}
+		
+		private static void putShort(byte[] byteBuffer, long memOff, int localOff, long v){
+			MyUnsafe.UNSAFE.putChar(byteBuffer, memOff + localOff, (char)(v >>> (localOff*8)));
+		}
+		private static char getShort(byte[] byteBuffer, long memOff){
+			return MyUnsafe.UNSAFE.getChar(byteBuffer, memOff);
+		}
+		private static long getShort(byte[] byteBuffer, long memOff, int localOff){
+			return ((long)MyUnsafe.UNSAFE.getChar(byteBuffer, memOff + localOff))<<(localOff*8);
+		}
+		
+		private static void putInt__(byte[] byteBuffer, long memOff, int localOff, long v){
+			MyUnsafe.UNSAFE.putInt(byteBuffer, memOff + localOff, (int)(v >>> (localOff*8)));
+		}
+		private static long getInt__(byte[] byteBuffer, long memOff, int localOff){
+			return Integer.toUnsignedLong(MyUnsafe.UNSAFE.getInt(byteBuffer, memOff + localOff))<<(localOff*8);
+		}
+		
+		private static void writeInt3(byte[] byteBuffer, int off, int v){
+			var memOff = mem(byteBuffer, off, 3);
+			putShort(byteBuffer, memOff, 0, v);
+			putByte_(byteBuffer, memOff, 2, v);
+		}
+		private static void writeInt5(byte[] byteBuffer, int off, long v){
+			var memOff = mem(byteBuffer, off, 5);
+			putInt__(byteBuffer, memOff, 0, v);
+			putByte_(byteBuffer, memOff, 4, v);
+		}
+		private static void writeInt6(byte[] byteBuffer, int off, long v){
+			var memOff = mem(byteBuffer, off, 6);
+			putInt__(byteBuffer, memOff, 0, v);
+			putShort(byteBuffer, memOff, 4, v);
+		}
+		private static void writeInt7(byte[] byteBuffer, int off, long v){
+			var memOff = mem(byteBuffer, off, 7);
+			putInt__(byteBuffer, memOff, 0, v);
+			putShort(byteBuffer, memOff, 4, v);
+			putByte_(byteBuffer, memOff, 6, v);
+		}
+		
+		
+		private static int readUnsignedInt3(byte[] byteBuffer, int offset){
+			var memOff = mem(byteBuffer, offset, 3);
+			return getShort(byteBuffer, memOff)|
+			       getByte_(byteBuffer, memOff + 2)<<16;
+		}
+		private static long readUnsignedInt5(byte[] byteBuffer, int offset){
+			var memOff = mem(byteBuffer, offset, 5);
+			return getInt__(byteBuffer, memOff, 0)|
+			       getByte_(byteBuffer, memOff, 4);
+		}
+		private static long readUnsignedInt6(byte[] byteBuffer, int offset){
+			var memOff = mem(byteBuffer, offset, 6);
+			return getInt__(byteBuffer, memOff, 0)|
+			       getShort(byteBuffer, memOff, 4);
+		}
+		private static long readUnsignedInt7(byte[] byteBuffer, int offset){
+			var memOff = mem(byteBuffer, offset, 7);
+			return getInt__(byteBuffer, memOff, 0)|
+			       getShort(byteBuffer, memOff, 4)|
+			       getByte_(byteBuffer, memOff, 6);
+		}
+	}
+	
+	private static final class VarHandleView{
+		private static void writeInt3(byte[] byteBuffer, int off, int v){
+			writeInt2(byteBuffer, off, (short)v);
+			byteBuffer[off + 2] = (byte)((v >>> 16)&0xFF);
+		}
+		private static void writeInt5(byte[] byteBuffer, int off, long v){
+			writeInt4(byteBuffer, off, (int)v);
+			byteBuffer[off + 4] = (byte)(v >>> 32);
+		}
+		private static void writeInt6(byte[] byteBuffer, int off, long v){
+			writeInt4(byteBuffer, off, (int)v);
+			writeInt2(byteBuffer, off + 4, (short)(v >>> 32));
+		}
+		private static void writeInt7(byte[] byteBuffer, int off, long v){
+			writeInt4(byteBuffer, off, (int)(v));
+			writeInt2(byteBuffer, off + 4, (short)(v >>> 32));
+			byteBuffer[off + 6] = (byte)(v >>> 48);
+		}
+		
+		private static int readUnsignedInt3(byte[] byteBuffer, int offset){
+			return readUnsignedInt2(byteBuffer, offset)|
+			       ((byteBuffer[offset + 2]&0xFF)<<16);
+		}
+		private static long readUnsignedInt5(byte[] byteBuffer, int offset){
+			return Integer.toUnsignedLong(readInt4(byteBuffer, offset))|
+			       ((long)(byteBuffer[offset + 4]&0xFF)<<32);
+		}
+		private static long readUnsignedInt6(byte[] byteBuffer, int offset){
+			return Integer.toUnsignedLong(readInt4(byteBuffer, offset))|
+			       (((long)readUnsignedInt2(byteBuffer, offset + 4))<<32);
+		}
+		private static long readUnsignedInt7(byte[] byteBuffer, int offset){
+			return Integer.toUnsignedLong(readInt4(byteBuffer, offset))|
+			       (((long)readUnsignedInt2(byteBuffer, offset + 4))<<32)|
+			       ((long)(byteBuffer[offset + 6]&0xFF)<<48);
+		}
+	}
+	
+	private static final boolean UNS = ConfigDefs.FIELD_ACCESS_TYPE.resolve() == FieldCompiler.AccessType.UNSAFE;
 	
 	private static final VarHandle SHORT_VIEW  = var(short.class);
 	private static final VarHandle CHAR_VIEW   = var(char.class);
@@ -23,25 +148,21 @@ public final class BBView{
 	public static void writeChar2(byte[] byteBuffer, int off, char v){ CHAR_VIEW.set(byteBuffer, off, v); }
 	public static void writeInt2(byte[] byteBuffer, int off, short v){ SHORT_VIEW.set(byteBuffer, off, v); }
 	public static void writeInt3(byte[] byteBuffer, int off, int v){
-		byteBuffer[off + 0] = (byte)((v >>> 0)&0xFF);
-		byteBuffer[off + 1] = (byte)((v >>> 8)&0xFF);
-		byteBuffer[off + 2] = (byte)((v >>> 16)&0xFF);
+		if(UNS) UnsafeView.writeInt3(byteBuffer, off, v);
+		else VarHandleView.writeInt3(byteBuffer, off, v);
 	}
 	public static void writeInt4(byte[] byteBuffer, int off, int v){ INT_VIEW.set(byteBuffer, off, v); }
 	public static void writeInt5(byte[] byteBuffer, int off, long v){
-		byteBuffer[off + 0] = (byte)(v >>> 0);
-		byteBuffer[off + 1] = (byte)(v >>> 8);
-		byteBuffer[off + 2] = (byte)(v >>> 16);
-		byteBuffer[off + 3] = (byte)(v >>> 24);
-		byteBuffer[off + 4] = (byte)(v >>> 32);
+		if(UNS) UnsafeView.writeInt5(byteBuffer, off, v);
+		else VarHandleView.writeInt5(byteBuffer, off, v);
 	}
 	public static void writeInt6(byte[] byteBuffer, int off, long v){
-		byteBuffer[off + 0] = (byte)(v >>> 0);
-		byteBuffer[off + 1] = (byte)(v >>> 8);
-		byteBuffer[off + 2] = (byte)(v >>> 16);
-		byteBuffer[off + 3] = (byte)(v >>> 24);
-		byteBuffer[off + 4] = (byte)(v >>> 32);
-		byteBuffer[off + 5] = (byte)(v >>> 40);
+		if(UNS) UnsafeView.writeInt6(byteBuffer, off, v);
+		else VarHandleView.writeInt6(byteBuffer, off, v);
+	}
+	public static void writeInt7(byte[] byteBuffer, int off, long v){
+		if(UNS) UnsafeView.writeInt7(byteBuffer, off, v);
+		else VarHandleView.writeInt7(byteBuffer, off, v);
 	}
 	public static void writeInt8(byte[] byteBuffer, int off, long v)    { LONG_VIEW.set(byteBuffer, off, v); }
 	public static void writeFloat4(byte[] byteBuffer, int off, float v) { FLOAT_VIEW.set(byteBuffer, off, v); }
@@ -53,25 +174,21 @@ public final class BBView{
 	public static short readInt2(byte[] byteBuffer, int offset)      { return (short)SHORT_VIEW.get(byteBuffer, offset); }
 	public static int readUnsignedInt2(byte[] byteBuffer, int offset){ return readChar2(byteBuffer, offset); }
 	public static int readUnsignedInt3(byte[] byteBuffer, int offset){
-		return (((byteBuffer[offset + 0]&255)<<0) +
-		        ((byteBuffer[offset + 1]&255)<<8) +
-		        ((byteBuffer[offset + 2]&255)<<16));
+		if(UNS) return UnsafeView.readUnsignedInt3(byteBuffer, offset);
+		else return VarHandleView.readUnsignedInt3(byteBuffer, offset);
 	}
 	public static int readInt4(byte[] byteBuffer, int offset){ return (int)INT_VIEW.get(byteBuffer, offset); }
 	public static long readUnsignedInt5(byte[] byteBuffer, int offset){
-		return (((long)(byteBuffer[offset + 0]&255)<<0) +
-		        ((long)(byteBuffer[offset + 1]&255)<<8) +
-		        ((long)(byteBuffer[offset + 2]&255)<<16) +
-		        ((long)(byteBuffer[offset + 3]&255)<<24) +
-		        ((long)(byteBuffer[offset + 4]&255)<<32));
+		if(UNS) return UnsafeView.readUnsignedInt5(byteBuffer, offset);
+		else return VarHandleView.readUnsignedInt5(byteBuffer, offset);
 	}
 	public static long readUnsignedInt6(byte[] byteBuffer, int offset){
-		return (((long)(byteBuffer[offset + 0]&255)<<0) +
-		        ((long)(byteBuffer[offset + 1]&255)<<8) +
-		        ((long)(byteBuffer[offset + 2]&255)<<16) +
-		        ((long)(byteBuffer[offset + 3]&255)<<24) +
-		        ((long)(byteBuffer[offset + 4]&255)<<32) +
-		        ((long)(byteBuffer[offset + 5]&255)<<40));
+		if(UNS) return UnsafeView.readUnsignedInt6(byteBuffer, offset);
+		else return VarHandleView.readUnsignedInt6(byteBuffer, offset);
+	}
+	public static long readUnsignedInt7(byte[] byteBuffer, int offset){
+		if(UNS) return UnsafeView.readUnsignedInt7(byteBuffer, offset);
+		else return VarHandleView.readUnsignedInt7(byteBuffer, offset);
 	}
 	public static long readInt8(byte[] byteBuffer, int offset) { return (long)LONG_VIEW.get(byteBuffer, offset); }
 	public static float readFloat4(byte[] byteBuffer, int off) { return (float)FLOAT_VIEW.get(byteBuffer, off); }
