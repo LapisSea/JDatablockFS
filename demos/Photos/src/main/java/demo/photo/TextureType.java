70b5c28a469aa5b6d349ce932ab0d5218254d132
LapisSea <officiallapisseamail@gmail.com>
2025-01-23 08:36:47 +0100

    Cleanup

--- demos/Photos/src/main/java/demo/photo/TextureType.java
+++ demos/Photos/src/main/java/demo/photo/TextureType.java
@@ -1,17 +1,19 @@
 package demo.photo;
 
+import com.lapissea.dfs.utils.iterableplus.Iters;
+
 import java.util.function.Predicate;
 
 public enum TextureType{
-	COLOR(str -> "COLOR".startsWith(str) || "DIFFUSE".startsWith(str) || str.equals("BASE_COLOR") || str.equals("BASECOLOR") || str.equals("ALBEDO") || str.matches("^VAR[0-9]*$") || str.matches("^COL_VAR[0-9]*$")),
-	DISPLACE(str -> "DISPLACEMENT".startsWith(str) || "HEIGHT".equals(str)),
+	COLOR(str -> "COLOR".startsWith(str) || "DIFFUSE".startsWith(str) || Iters.of("BASE_COLOR", "BASECOLOR", "ALBEDO").anyEquals(str) || str.matches("^VAR[0-9]*$") || str.matches("^COL_VAR[0-9]*$")),
+	DISPLACE(Iters.of("DISPLACEMENT", "HEIGHT")::anyEquals),
 	NORMAL(str -> "NORMAL".startsWith(str) || str.matches("^NRM[0-9]*$")),
-	ROUGHNESS(str -> "ROUGHNESS".startsWith(str) || "RGH".equals(str)),
-	AMBIENT_OCCLUSION("AO"::equals),
+	ROUGHNESS(Iters.of("ROUGHNESS", "RGH")::anyEquals),
+	AMBIENT_OCCLUSION(Iters.of("AO", "AMBIENTOCCLUSION", "AMBIENT_OCCLUSION")::anyEquals),
 	ROUGH_AMBIENT_OCCLUSION("ROUGH_AO"::equals),
 	MASK(str -> str.matches("^MASK[0-9]*$") || "OPACITY".equals(str)),
 	ID("ID"::equals),
-	METALNESS(str -> "METALNESS".startsWith(str) || "METALLIC".equals(str)),
+	METALNESS(Iters.of("METALNESS", "METALLIC")::anyEquals),
 	EMISSION("EMISSION"::startsWith),
 	SPECULAR("SPECULAR"::startsWith),
 	BUMP("BUMP"::equals),
