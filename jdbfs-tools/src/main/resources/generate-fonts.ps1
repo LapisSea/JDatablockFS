c7de72253c2c843366ef0d906ede3dfd75e1a125
LapisSea <officiallapisseamail@gmail.com>
2025-03-24 20:19:46 +0100

    Update MSDF atlases

--- jdbfs-tools/src/main/resources/generate-fonts.ps1
+++ jdbfs-tools/src/main/resources/generate-fonts.ps1
@@ -2,6 +2,6 @@
 get-childitem '.' -recurse -force -Name -Filter *font.ttf | ForEach-Object{
     $File = $_
     $par=($File -split '\\')[0..(($path -split '\\').count)] -join '\'
-    Start-Process 'msdf-atlas-gen.exe' -ArgumentList "-font $File -size 40 -imageout $par\atlas.png -json $par\atlas.json -charset charset.txt -potr"
+    Start-Process 'msdf-atlas-gen.exe' -ArgumentList "-font $File -size 45 -pxrange 3 -pxpadding 1 -imageout atlas.png -json atlas.json -charset charset.txt -potr -coloringstrategy distance"
     Start-Process 'msdf-atlas-gen.exe' -ArgumentList "-font $File -size 100 -imageout $par\mask.png -json $par\mask.json -charset charset.txt -potr -type softmask"
 }
