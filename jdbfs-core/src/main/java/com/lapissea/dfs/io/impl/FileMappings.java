a7adec7a70d6e195e388b0a3c009690c74c3eca9
LapisSea <officiallapisseamail@gmail.com>
2025-05-17 17:11:53 +0200

    Reimplement FileMappings with java.lang.foreign and remove need for Unsafe

--- jdbfs-core/src/main/java/com/lapissea/dfs/io/impl/FileMappings.java
+++ jdbfs-core/src/main/java/com/lapissea/dfs/io/impl/FileMappings.java
@@ -7,26 +7,36 @@ import com.lapissea.util.UtilL;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.lang.ref.Cleaner;
 import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.FileChannel;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static com.lapissea.dfs.config.GlobalConfig.DEBUG_VALIDATION;
-import static com.lapissea.dfs.internal.MyUnsafe.UNSAFE;
 
 @SuppressWarnings("unchecked")
 final class FileMappings implements Closeable{
 	
+	private static final Cleaner         CLEANER      = Cleaner.create();
+	private static final ExecutorService CLEAN_WORKER = Executors.newVirtualThreadPerTaskExecutor();
+	
 	public static final int MAX_CHUNK_SIZE    = 128*1024*1024;
 	public static final int MAX_SMALL_MAPPING = 32;
+	
+	private static void gcRegister(Object object, Runnable onClean){
+		CLEANER.register(object, () -> CLEAN_WORKER.execute(onClean));
+	}
 	
 	private final FileChannel fileChannel;
 	private       boolean     closed;
@@ -34,29 +44,95 @@ final class FileMappings implements Closeable{
 	private final boolean readOnly;
 	private       long    size;
 	
-	private       Reference<MappedByteBuffer>[]          mappings = new Reference[1];
-	private       Map<Long, Reference<MappedByteBuffer>> largeMappings;//null by default as most of the time it will never be used
+	private static final class AllocatedMapping implements AutoCloseable{
+		private final MemorySegment segment;
+		private final ByteBuffer    bb;
+		
+		
+		private final Runnable closer;
+		
+		private AllocatedMapping(Arena arena, MemorySegment segment){
+			this.segment = segment;
+			bb = segment.asByteBuffer();
+			
+			gcRegister(this, closer = new Runnable(){
+				private boolean closed;
+				@Override
+				public synchronized void run(){
+					if(closed) return;
+					try{
+						segment.force();
+						segment.unload();
+					}catch(UncheckedIOException e){
+						if(!tryFlush(segment)){
+							throw e;
+						}
+					}
+					arena.close();
+					closed = true;
+				}
+			});
+		}
+		
+		@Override
+		public String toString(){
+			return segment.toString();
+		}
+		@Override
+		public void close(){
+			closer.run();
+		}
+		
+		private static boolean tryFlush(MemorySegment segment){
+			for(int attempts = 0; attempts<15; attempts++){
+				try{
+					segment.force();
+					segment.unload();
+					return true;
+				}catch(UncheckedIOException e){
+					if(e.getMessage().contains("Insufficient system resources")){
+						if(attempts>1) Log.warn("Failing mapped file flushing! Attempt {}/15, Reason: {}", attempts, e.getCause().getMessage());
+						UtilL.sleep(attempts*attempts*10L);
+					}else{
+						throw e;
+					}
+				}
+			}
+			return false;
+		}
+	}
+	
+	private       Reference<AllocatedMapping>[]          mappings = new Reference[1];
+	private       Map<Long, Reference<AllocatedMapping>> largeMappings;//null by default as most of the time it will never be used
 	private final ReadWriteClosableLock                  lock     = ReadWriteClosableLock.reentrant();
 	
 	FileMappings(FileChannel fileChannel, boolean readOnly) throws IOException{
 		this.fileChannel = fileChannel;
 		this.readOnly = readOnly;
 		size = fileChannel.size();
+		
+		gcRegister(this, () -> {
+			try{
+				fileChannel.close();
+			}catch(IOException e){
+				//Nothing to do
+			}
+		});
 	}
 	
 	private void checkClosed() throws IOException{
 		if(closed) throw new ClosedChannelException();
 	}
 	
-	private Reference<MappedByteBuffer> removeLargeMapping(long index){
+	private Reference<AllocatedMapping> removeLargeMapping(long index){
 		var lm = largeMappings;
 		return lm == null? null : lm.remove(index);
 	}
-	private Reference<MappedByteBuffer> getLargeMapping(long index){
+	private Reference<AllocatedMapping> getLargeMapping(long index){
 		var lm = largeMappings;
 		return lm == null? null : lm.get(index);
 	}
-	private void putLargeMapping(long index, Reference<MappedByteBuffer> ref){
+	private void putLargeMapping(long index, Reference<AllocatedMapping> ref){
 		var lm = largeMappings;
 		if(lm == null) lm = largeMappings = new HashMap<>();
 		lm.put(index, ref);
@@ -69,11 +145,14 @@ final class FileMappings implements Closeable{
 	@Override
 	public void close() throws IOException{
 		try(var ignore = lock.write()){
-			checkClosed();
-			closed = true;
-			clearMappings();
-			fileChannel.close();
+			closeSync();
 		}
+	}
+	private void closeSync() throws IOException{
+		checkClosed();
+		closed = true;
+		clearMappings();
+		fileChannel.close();
 	}
 	
 	private final ByteBuffer zero = ByteBuffer.wrap(new byte[]{0});
@@ -102,39 +181,43 @@ final class FileMappings implements Closeable{
 	
 	private void removeMapping(long index){
 		if(index<mappings.length){
-			runIfSome(mappings[(int)index], FileMappings::unmap);
+			runIfSome(mappings[(int)index], AllocatedMapping::close);
 			mappings[(int)index] = null;
 		}else{
-			runIfSome(removeLargeMapping(index), FileMappings::unmap);
+			runIfSome(removeLargeMapping(index), AllocatedMapping::close);
 		}
 	}
 	
-	private MappedByteBuffer makeMapping(long index) throws IOException{
+	private AllocatedMapping makeMapping(long index) throws IOException{
 		try(var ignore = lock.write()){
 			checkClosed();
-			if(index<MAX_SMALL_MAPPING){
-				var iIndex = (int)index;
-				if(mappings.length<=iIndex){
-					mappings = Arrays.copyOf(mappings, iIndex + 1);
-				}
-				var mapping = unwrap(mappings[iIndex]);
-				if(mapping != null){
-					return mapping;
-				}
-				var val = mapIdx(index);
-				mappings[iIndex] = wrap(val);
-				return val;
-			}
-			
-			var mapping = unwrap(getLargeMapping(index));
-			if(mapping == null){
-				var ref = wrap(mapping = mapIdx(index));
-				putLargeMapping(index, ref);
-			}
-			return mapping;
+			if(index<MAX_SMALL_MAPPING) return makeSmallMapping((int)index);
+			return makeLargeMapping(index);
 		}
 	}
-	private MappedByteBuffer getMapping(long index){
+	private AllocatedMapping makeSmallMapping(int index) throws IOException{
+		if(mappings.length>index){
+			var mapping = unwrap(mappings[index]);
+			if(mapping != null){
+				return mapping;
+			}
+		}else{
+			mappings = Arrays.copyOf(mappings, index + 1);
+		}
+		var val = mapIdx(index);
+		mappings[index] = wrap(val);
+		return val;
+	}
+	private AllocatedMapping makeLargeMapping(long index) throws IOException{
+		var mapping = unwrap(getLargeMapping(index));
+		if(mapping == null){
+			var ref = wrap(mapping = mapIdx(index));
+			putLargeMapping(index, ref);
+		}
+		return mapping;
+	}
+	
+	private AllocatedMapping getMapping(long index){
 		try(var ignore = lock.read()){
 			if(index<MAX_SMALL_MAPPING){
 				var iIndex = (int)index;
@@ -148,64 +231,39 @@ final class FileMappings implements Closeable{
 		}
 	}
 	
-	public <T> T onMapping(long index, Function<MappedByteBuffer, T> consumer) throws IOException{
+	public <T> T onMapping(long index, Function<ByteBuffer, T> consumer) throws IOException{
 		checkClosed();
 		var mapping = getMapping(index);
 		if(mapping == null){
 			mapping = makeMapping(index);
 		}
-		var res = consumer.apply(mapping);
+		var res = consumer.apply(mapping.bb);
 		if(DEBUG_VALIDATION && res == mapping) throw new IllegalStateException("Do not leak the MappedByteBuffer");
 		return res;
 	}
 	
 	/// /////////////////////
 	
-	private MappedByteBuffer mapIdx(long index) throws IOException{
+	private AllocatedMapping mapIdx(long index) throws IOException{
 		var chunkStart = index*MAX_CHUNK_SIZE;
 		var remaining  = (int)Math.min(size - chunkStart, MAX_CHUNK_SIZE);
 		
 		return map(chunkStart, remaining);
 	}
-	private MappedByteBuffer map(long pos, int len) throws IOException{
-		var mode = readOnly? FileChannel.MapMode.READ_ONLY : FileChannel.MapMode.READ_WRITE;
-		return fileChannel.map(mode, pos, len);
-	}
-	private static void unmap(MappedByteBuffer value){
-		try{
-			value.force();
-		}catch(UncheckedIOException e){
-			if(!tryFlush(value)){
-				throw e;
-			}
-		}
-		UNSAFE.invokeCleaner(value);
-	}
-	
-	private static boolean tryFlush(MappedByteBuffer value){
-		for(int attempts = 0; attempts<15; attempts++){
-			try{
-				value.force();
-				return true;
-			}catch(UncheckedIOException e){
-				if(e.getMessage().contains("Insufficient system resources")){
-					if(attempts>1) Log.warn("Failing mapped file flushing! Attempt {}/15, Reason: {}", attempts, e.getCause().getMessage());
-					UtilL.sleep(attempts*attempts*10L);
-				}else{
-					throw e;
-				}
-			}
-		}
-		return false;
+	private AllocatedMapping map(long pos, int len) throws IOException{
+		var mode   = readOnly? FileChannel.MapMode.READ_ONLY : FileChannel.MapMode.READ_WRITE;
+		var arena  = Arena.ofShared();
+		var mapped = fileChannel.map(mode, pos, len, arena);
+		return new AllocatedMapping(arena, mapped);
 	}
 	
 	private void clearMappings(){
-		iterateMappings(FileMappings::unmap);
+		iterateMappings(AllocatedMapping::close);
 		mappings = new Reference[1];
 		largeMappings = null;
 	}
 	
-	private void iterateMappings(Consumer<MappedByteBuffer> consumer){
+	private void iterateMappings(Consumer<AllocatedMapping> consumer){
 		for(var ref : mappings){
 			runIfSome(ref, consumer);
 		}
@@ -217,16 +275,16 @@ final class FileMappings implements Closeable{
 		}
 	}
 	
-	private static void runIfSome(Reference<MappedByteBuffer> wrapped, Consumer<MappedByteBuffer> consumer){
+	private static void runIfSome(Reference<AllocatedMapping> wrapped, Consumer<AllocatedMapping> consumer){
 		if(wrapped == null) return;
 		var val = wrapped.get();
 		if(val == null) return;
 		consumer.accept(val);
 	}
-	private static MappedByteBuffer unwrap(Reference<MappedByteBuffer> wrapped){
+	private static AllocatedMapping unwrap(Reference<AllocatedMapping> wrapped){
 		return wrapped == null? null : wrapped.get();
 	}
-	private static Reference<MappedByteBuffer> wrap(MappedByteBuffer raw){
+	private static Reference<AllocatedMapping> wrap(AllocatedMapping raw){
 		return new WeakReference<>(raw);
 	}
 }
