f8592850c1b6fdc94a6c72f670e60360e6ca35db
LapisSea <officiallapisseamail@gmail.com>
2025-06-05 00:04:51 +0200

    Move STBImage image load to its own class

---
+++ jdbfs-tools/src/main/java/com/lapissea/dfs/tools/newlogger/display/ColorRGBA8.java
@@ -0,0 +1,72 @@
+package com.lapissea.dfs.tools.newlogger.display;
+
+import com.lapissea.dfs.logging.Log;
+import org.lwjgl.stb.STBImage;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Objects;
+
+public class ColorRGBA8 implements AutoCloseable{
+	
+	public static ColorRGBA8 fromJar(String path) throws IOException{
+		ByteBuffer imageFile = null;
+		try{
+			imageFile = VUtils.nativeMemCopy(VUtils.readResource(path));
+			return fromFileData(path, imageFile);
+		}finally{
+			if(imageFile != null) MemoryUtil.memFree(imageFile);
+		}
+	}
+	public static ColorRGBA8 fromFile(File file) throws IOException{
+		try(var io = new RandomAccessFile(file, "r")){
+			var imageFile = io.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, io.length());
+			return fromFileData(file.toString(), imageFile);
+		}
+	}
+	public static ColorRGBA8 fromFileData(String name, ByteBuffer imageFile) throws IOException{
+		ByteBuffer pixels = null;
+		try(var stack = MemoryStack.stackPush();){
+			IntBuffer xB = stack.mallocInt(1), yB = stack.mallocInt(1), channelsB = stack.mallocInt(1);
+			pixels = STBImage.stbi_load_from_memory(imageFile, xB, yB, channelsB, STBImage.STBI_rgb_alpha);
+			
+			if(pixels == null) throw new IOException("Image \"" + name + "\" was invalid");
+			
+			int width  = xB.get(0);
+			int height = yB.get(0);
+			
+			Log.info("loaded image " + name + " " + xB.get(0) + "x" + yB.get(0));
+			
+			var pixOwn = pixels;
+			pixels = null;
+			return new ColorRGBA8(pixOwn, width, height);
+		}finally{
+			if(pixels != null) MemoryUtil.memFree(pixels);
+		}
+	}
+	
+	private      ByteBuffer pixels;
+	public final int        width, height;
+	
+	public ColorRGBA8(ByteBuffer pixels, int width, int height){
+		this.pixels = Objects.requireNonNull(pixels);
+		this.width = width;
+		this.height = height;
+		
+	}
+	
+	public ByteBuffer getPixels(){
+		return pixels;
+	}
+	@Override
+	public void close(){
+		MemoryUtil.memFree(pixels);
+		pixels = null;
+	}
+}
--- jdbfs-tools/src/main/java/com/lapissea/dfs/tools/newlogger/display/vk/VulkanTexture.java
+++ jdbfs-tools/src/main/java/com/lapissea/dfs/tools/newlogger/display/vk/VulkanTexture.java
@@ -1,20 +1,14 @@
 package com.lapissea.dfs.tools.newlogger.display.vk;
 
-import com.lapissea.dfs.logging.Log;
-import com.lapissea.dfs.tools.newlogger.display.VUtils;
+import com.lapissea.dfs.tools.newlogger.display.ColorRGBA8;
 import com.lapissea.dfs.tools.newlogger.display.VulkanCodeException;
 import com.lapissea.dfs.tools.newlogger.display.vk.enums.VkFormat;
 import com.lapissea.dfs.tools.newlogger.display.vk.wrap.VkDeviceMemory;
 import com.lapissea.dfs.tools.newlogger.display.vk.wrap.VkImage;
 import com.lapissea.dfs.tools.newlogger.display.vk.wrap.VkImageView;
 import com.lapissea.dfs.tools.newlogger.display.vk.wrap.VkSampler;
-import org.lwjgl.stb.STBImage;
-import org.lwjgl.system.MemoryStack;
-import org.lwjgl.system.MemoryUtil;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
@@ -22,34 +16,13 @@ public class VulkanTexture implements VulkanResource{
 	
 	public static CompletableFuture<VulkanTexture> loadTexture(String name, boolean generateMips, Supplier<VulkanCore> coreGet){
 		return CompletableFuture.supplyAsync(() -> {
-			
-			ByteBuffer pixels = null, image = null;
-			try(var stack = MemoryStack.stackPush()){
-				image = VUtils.nativeMemCopy(VUtils.readResource(name));
-				
-				IntBuffer xB = stack.mallocInt(1), yB = stack.mallocInt(1), channelsB = stack.mallocInt(1);
-				pixels = STBImage.stbi_load_from_memory(image, xB, yB, channelsB, STBImage.STBI_rgb_alpha);
-				
-				if(pixels == null) throw new IOException("Image \"" + name + "\" was invalid");
-				
-				int width  = xB.get(0);
-				int height = yB.get(0);
-				
-				Log.info("loaded image " + name + " " + xB.get(0) + "x" + yB.get(0));
-				
-				var levels = generateMips? (int)Math.floor(Math.log(Math.max(width, height))/Math.log(2)) + 1 : 1;
-				
-				var core = coreGet.get();
-				
-				return core.uploadTexture(width, height, pixels, VkFormat.R8G8B8A8_UNORM, levels);
-				
+			try(var image = ColorRGBA8.fromJar(name)){
+				var levels = generateMips? (int)Math.floor(Math.log(Math.max(image.width, image.height))/Math.log(2)) + 1 : 1;
+				var core   = coreGet.get();
+				return core.uploadTexture(image.width, image.height, image.getPixels(), VkFormat.R8G8B8A8_UNORM, levels);
 			}catch(IOException|VulkanCodeException e){
 				throw new RuntimeException("Failed to load image", e);
-			}finally{
-				if(image != null) MemoryUtil.memFree(image);
-				if(pixels != null) MemoryUtil.memFree(pixels);
 			}
-			
 		});
 	}
 	
