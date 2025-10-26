331d532e0e1e4da8b9f43659f8407b9c6bfbab52
LapisSea <officiallapisseamail@gmail.com>
2025-10-26 20:33:20 +0100

    RangeMessageSpace: delay tree building, scene faster build

--- jdbfs-tools/src/main/java/com/lapissea/dfs/tools/newlogger/display/renderers/grid/RangeMessageSpace.java
+++ jdbfs-tools/src/main/java/com/lapissea/dfs/tools/newlogger/display/renderers/grid/RangeMessageSpace.java
@@ -2,8 +2,6 @@ package com.lapissea.dfs.tools.newlogger.display.renderers.grid;
 
 import com.lapissea.dfs.tools.DrawUtils.Range;
 import com.lapissea.dfs.utils.iterableplus.Iters;
-import com.lapissea.util.ShouldNeverHappenError;
-import com.lapissea.util.UtilL;
 
 import java.awt.Color;
 import java.util.ArrayList;
@@ -22,7 +20,7 @@ public class RangeMessageSpace{
 		
 	}
 	
-	private record Node(Range range, List<Node> children, Message val) implements Comparable<Node>{
+	private record Node(Range range, ArrayList<Node> children, Message val) implements Comparable<Node>{
 		private Node(Message val)             { this(val.range, new ArrayList<>(), val); }
 		private Node(Range range, Message val){ this(range, new ArrayList<>(), val); }
 		@Override
@@ -33,22 +31,6 @@ public class RangeMessageSpace{
 			for(Node child : children){
 				child.all(dst);
 			}
-		}
-		
-		private boolean add(Node node){
-			if(!node.range.isWithin(range)){
-				return false;
-			}
-			
-			relax();
-			for(var branch : children){
-				if(branch.add(node)){
-					return true;
-				}
-			}
-			children.removeIf(node::add);
-			UtilL.addRemainSorted(children, node);
-			return true;
 		}
 		
 		private void relax(){
@@ -64,18 +46,16 @@ public class RangeMessageSpace{
 			Collections.sort(ch);
 			
 			var split = ch.size()/2;
-			var l1    = new ArrayList<>(ch.subList(0, split));
-			var l2    = new ArrayList<>(ch.subList(split, ch.size()));
+			var l1    = ch.subList(0, split);
+			var l2    = ch.subList(split, ch.size());
 			
 			children.clear();
-			children.add(new Node(calcRange(l1), Iters.from(l1).map(Node::new).toModList(), null));
-			children.add(new Node(calcRange(l2), Iters.from(l2).map(Node::new).toModList(), null));
-			for(Node child : children){
-				child.relax();
-			}
+			children.add(new Node(calcRange(l1), Iters.from(l1).map(Node::new).asCollection(ArrayList::new), null));
+			children.add(new Node(calcRange(l2), Iters.from(l2).map(Node::new).asCollection(ArrayList::new), null));
+			children.trimToSize();
 		}
 		
-		private Range calcRange(ArrayList<Message> l1){
+		private Range calcRange(List<Message> l1){
 			return new Range(
 				Iters.from(l1).map(Message::range).mapToLong(Range::from).min(0),
 				Iters.from(l1).map(Message::range).mapToLong(Range::to).max(0)
@@ -87,29 +67,30 @@ public class RangeMessageSpace{
 			if(val != null){
 				result.add(val);
 			}
-			var count = 0;
+			relax();
+			var count = 1;
 			for(var child : children){
 				count += child.collect(p, result);
 			}
 			return count;
 		}
-		
-		private String toTreeString(){
-			return toTreeString(0);
-		}
-		private String toTreeString(int indent){
-			StringBuilder sb = new StringBuilder();
-			sb.append("  ".repeat(indent))
-			  .append(val == null? "" : val.message)
-			  .append(" [")
-			  .append(range)
-			  .append("]")
-			  .append('\n');
-			for(var child : children){
-				sb.append(child.toTreeString(indent + 1));
-			}
-			return sb.toString();
-		}
+
+//		private String toTreeString(){
+//			return toTreeString(0);
+//		}
+//		private String toTreeString(int indent){
+//			StringBuilder sb = new StringBuilder();
+//			sb.append("  ".repeat(indent))
+//			  .append(val == null? "" : val.message)
+//			  .append(" [")
+//			  .append(range)
+//			  .append("]")
+//			  .append('\n');
+//			for(var child : children){
+//				sb.append(child.toTreeString(indent + 1));
+//			}
+//			return sb.toString();
+//		}
 	}
 	
 	public record Message(String message, Range range, HoverEffect hoverEffect) implements Comparable<Message>{
@@ -131,9 +112,7 @@ public class RangeMessageSpace{
 	}
 	public void add(String message, Range range, HoverEffect hoverEffect){
 		var msg = new Message(message, range, hoverEffect);
-		if(!root.add(new Node(range, msg))){
-			throw new ShouldNeverHappenError("Must be able to add to root");
-		}
+		root.children.add(new Node(range, msg));
 	}
 	
 	public List<Message> collect(long hoverIndex){
